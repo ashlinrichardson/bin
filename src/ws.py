9a04354937b924aa3492d5efc849d5a0f7c00039
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-02 22:05:37 -0700

    pep8
    pep8

--- src/ws.py
+++ src/ws.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
-# by arichardson 20170120 reimplemented 20170602
+''' by arichardson 20170120 reimplemented 20170602  '''
+
 import os
 import sys
 import ntpath
@@ -7,55 +8,54 @@ from fl0w3r import error, exists, printw
 
 args, max_spaces = sys.argv, 0
 if len(args) < 2:
-  error('ws.py: usage: ws.py [input file name].\n\t ws.py removes: i) all trailing right-whitespace ii) any systematic left-whitespace.');
+    msg = 'ws.py: usage: ws.py [input file name].\n\t'
+    error(msg + 'removes trailing right-space and systematic left-space')
+
 
 def f_base(fn):
-  return ntpath.basename(fn)
+    return ntpath.basename(fn)
 
 try:
-  fn = os.path.abspath(args[1].strip())
+    fn = os.path.abspath(args[1].strip())
 except:
-  error('could not parse filename')
+    error('could not parse filename')
 
 if not exists(fn):
-  error('file [' + str(fn) + '] not found.')
+    error('file [' + str(fn) + '] not found.')
 
-lines = open(fn).readlines();
+lines = open(fn).readlines()
 for i in range(0, len(lines)):
-  lines[i] = lines[i].rstrip()
-  l_i = lines[i]
-  l_spaces = len(l_i) - len(l_i.lstrip()) 
-  if(l_spaces > max_spaces):
-    max_spaces = l_spaces
+    lines[i] = lines[i].rstrip()
+    l_i = lines[i]
+    l_spaces = len(l_i) - len(l_i.lstrip())
+    if(l_spaces > max_spaces):
+        max_spaces = l_spaces
 
 min_spaces = max_spaces
-for i in range(0,len(lines)):
-  l_i = lines[i]
-  l_spaces = len(l_i) - len(l_i.lstrip())            
-  if(l_spaces < min_spaces):
-    if(l_i.strip() != ''):
-      min_spaces = l_spaces
+for i in range(0, len(lines)):
+    l_i = lines[i]
+    l_spaces = len(l_i) - len(l_i.lstrip())
+    if(l_spaces < min_spaces):
+        if(l_i.strip() != ''):
+            min_spaces = l_spaces
 
 print "min_spaces", min_spaces
 head, tail = ntpath.split(fn)
-head = head.strip()+'/'
-head = os.path.abspath(head)+'/'
-print 'head',head
+head = head.strip() + '/'
+head = os.path.abspath(head) + '/'
+print 'head', head
 bfn = head + '.' + f_base(fn)
-print 'bfn', bfn 
+print 'bfn', bfn
 
-# save a backup.
-open(bfn,'wb').write(open(fn).read())
+# save a backup
+open(bfn, 'wb').write(open(fn).read())
 printw(bfn)
 
 write_ln = []
-for i in range(0,len(lines)):
-  line = lines[i]
-  write_ln.append(line[min_spaces:])
+for i in range(0, len(lines)):
+    line = lines[i]
+    write_ln.append(line[min_spaces:])
 
 # write the result.
-open(fn,'wb').write(('\n').join( write_ln))
+open(fn, 'wb').write(('\n').join(write_ln))
 printw(fn)
-
-
-
