fc6b3a8fda6e77153839fc6269bf3dbeaf9ce4cc
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-02 21:43:02 -0700

    remove all trailing whitespace or any systematic left-whitespace
    remove all trailing whitespace or any systematic left-whitespace

---
+++ src/ws.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2.7
+# by arichardson 20170120 reimplemented 20170602
+import os
+import sys
+import ntpath
+from fl0w3r import error, exists, printw
+
+args, max_spaces = sys.argv, 0
+if len(args) < 2:
+  error('ws.py: usage: ws.py [input file name].\n\t ws.py removes: i) all trailing right-whitespace ii) any systematic left-whitespace.');
+
+def f_base(fn):
+  return ntpath.basename(fn)
+
+try:
+  fn = os.path.abspath(args[1].strip())
+except:
+  error('could not parse filename')
+
+if not exists(fn):
+  error('file [' + str(fn) + '] not found.')
+
+lines = open(fn).readlines();
+for i in range(0, len(lines)):
+  lines[i] = lines[i].rstrip()
+  l_i = lines[i]
+  l_spaces = len(l_i) - len(l_i.lstrip()) 
+  if(l_spaces > max_spaces):
+    max_spaces = l_spaces
+
+min_spaces = max_spaces
+for i in range(0,len(lines)):
+  l_i = lines[i]
+  l_spaces = len(l_i) - len(l_i.lstrip())            
+  if(l_spaces < min_spaces):
+    if(l_i.strip() != ''):
+      min_spaces = l_spaces
+
+print "min_spaces", min_spaces
+head, tail = ntpath.split(fn)
+head = head.strip()+'/'
+head = os.path.abspath(head)+'/'
+print 'head',head
+bfn = head + '.' + f_base(fn)
+print 'bfn', bfn 
+
+# save a backup.
+open(bfn,'wb').write(open(fn).read())
+printw(bfn)
+
+write_ln = []
+for i in range(0,len(lines)):
+  line = lines[i]
+  write_ln.append(line[min_spaces:])
+
+# write the result.
+open(fn,'wb').write(('\n').join( write_ln))
+printw(fn)
+
+
+
