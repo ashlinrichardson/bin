ce4c2f0eaed295fd104b92ac7d35e92cf8b33279
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-02 21:43:14 -0700

    tidy up
    tidy up

--- src/pdfcat.py
+++ src/pdfcat.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
-
-import os,sys
+import os
+import sys
+from fl0w3r import error, run
 
 args = sys.argv
+if len(args) < 4:
+  error('pdfcat.py.\nUsage: [file1] [file2] ... [filen] [outfilename.pdf]')
 
-if(len(args)<4):
-	print("pdfcat.py.\nUsage: [file1] [file2] ... [filen] [outfilename.pdf]\n");
-	sys.exit(1);
-
-files = args[1:];
-outfile = files[len(files)-1];
+files = args[1:]
+outfile, infile = files[-1], files[:-1]
 
 if os.path.exists(outfile):
-  print "error: output file already exists"
-  sys.exit(1)
+  error('output file already exists: aborting')
 
-infiles = files[:-1];
-s = "";
+s = ''
 for infile in infiles: 
-	s = s+infile+" ";
-print("input file names: "+s);#str(infiles));
-print("output file name:  "+str(outfile));
-# had -sPAPERSIZE=letter
-cmd = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="+outfile;
-#   2030book.pdf cover2.pdf  2030.pdf
-for infile in infiles: 
-	cmd = cmd + " "+infile 	
-print("\nexecuting command:");
-print("    "+cmd)
-a = os.system(cmd);
+	s += (infile + ' ')
 
+print("input file names: " + s)
+print("output file name:  " + str(outfile))
+
+cmd = "gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=" + outfile
+for infile in infiles: 
+	cmd += (" " + infile)
+run(cmd)
