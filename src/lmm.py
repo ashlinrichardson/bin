0204e5484c74421e980f608c73f6159a430f035f
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-06 22:20:53 -0700

    pep8
    pep8

--- src/lmm.py
+++ src/lmm.py
@@ -2,27 +2,23 @@
 '''want to know where your disk space is used?
 summarize file space usage, producing a grand total (du -c), with sorting
 '''
-import os, sys
-def msg():
-  print("lmm.py\n\tlmm.py\t\t\t\t(this defaults to current directory)\n\tlmm.py [path]\t\t\t\t(this allows a path to be specified.\n");
-  sys.exit(1);
-args = sys.argv
-pwd = os.path.abspath( os.popen("pwd").read().strip() );
-if(len(args)>1):
-  pwd = os.path.abspath( args[1] );
-  if( not( os.path.exists( pwd)) or not( os.path.isdir(pwd))):
-    msg();
-cmd = "du -c "+pwd
-a =os.popen(cmd).read().strip().split("\n");
-lines = [ ]
+import os
+import sys
+from fl0w3r import error, normpath
+args, pwd = sys.argv, normpath(os.getcwd())
+if len(args) > 1:
+    pwd = os.path.abspath(args[1])
+    if not os.path.exists(pwd) or not os.path.isdir(pwd):
+        error('lmm.py: use: lmm.py or lmm.py [path] (defaults to pwd)')
+cmd = "du -c " + pwd
+a = os.popen(cmd).read().strip().split('\n')
+lines = []
 for line in a:
-  #print line.strip();
-  ls = line.strip().split();
-  n = int(ls[0]);
-  rest = (" ".join(ls[-1:])).strip();
-  lines.append(  [n, line]);#rest] )
+    ls = line.strip().split()
+    n = int(ls[0])
+    rest = (" ".join(ls[-1:])).strip()
+    lines.append([n, line])
 lines.sort(key=lambda x: x[0])
 
 for line in lines:
-  print(str(line[0])+" "+line[1]);
-
+    print(str(line[0])+" "+line[1])
