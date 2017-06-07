0eb4034d4660db9ee759a2ec9e1aae5db2db439c
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-06 22:37:52 -0700

    tidy up
    tidy up

--- src/lmm.py
+++ src/lmm.py
@@ -4,21 +4,23 @@ summarize file space usage, producing a grand total (du -c), with sorting
 '''
 import os
 import sys
-from fl0w3r import error, normpath
+from fl0w3r import error, normpath, exists
 args, pwd = sys.argv, normpath(os.getcwd())
+print pwd
 if len(args) > 1:
-    pwd = os.path.abspath(args[1])
-    if not os.path.exists(pwd) or not os.path.isdir(pwd):
+    pwd = normpath(args[1])
+    if not exists(pwd) or not os.path.isdir(pwd):
         error('lmm.py: use: lmm.py or lmm.py [path] (defaults to pwd)')
 cmd = "du -c " + pwd
 a = os.popen(cmd).read().strip().split('\n')
 lines = []
 for line in a:
     ls = line.strip().split()
+    if len(ls) > 2:
+        error('paths with spaces not supported')
     n = int(ls[0])
-    rest = (" ".join(ls[-1:])).strip()
-    lines.append([n, line])
+    lines.append([n, normpath(ls[1])])
 lines.sort(key=lambda x: x[0])
 
 for line in lines:
-    print(str(line[0])+" "+line[1])
+    print str(line[0]) + ' ' + line[1]
