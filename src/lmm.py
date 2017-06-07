e61d5990338d4714bee2cb81d7270dfc479e76ab
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-06 22:41:03 -0700

    Revert "tidy up"
    This reverts commit db19d4a804529a2051059c42aebda27d2fb03360.

--- src/lmm.py
+++ src/lmm.py
@@ -11,11 +11,15 @@ if len(args) > 1:
     pwd = normpath(args[1])
     if not exists(pwd) or not os.path.isdir(pwd):
         error('lmm.py: use: lmm.py or lmm.py [path] (defaults to pwd)')
+cmd = "du -c " + pwd
+a = os.popen(cmd).read().strip().split('\n')
 lines = []
-for line in os.popen("du -c " + pwd).read().strip().split('\n'):
-    if len(line.strip().split()) > 2:
+for line in a:
+    ls = line.strip().split()
+    if len(ls) > 2:
         error('paths with spaces not supported')
-    lines.append([int(ls[0]), normpath(ls[1])])
+    n = int(ls[0])
+    lines.append([n, normpath(ls[1])])
 lines.sort(key=lambda x: x[0])
 
 for line in lines:
