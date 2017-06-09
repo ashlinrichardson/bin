8df6df3922f55093b630c5c1df6489a22731bd11
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-08 21:05:05 -0700

    add multi-look and htrim
    add multi-look and htrim

--- src/absimag.py
+++ src/absimag.py
@@ -18,8 +18,8 @@ nlin, ncol = read_config(cwd + 'config.txt')
 bins = os.popen('ls -1 ' + cwd + '*.bin').readlines()
 
 for b in bins:
-    b = normpath(b)
-    fn = b.strip().split("/")[-1]
+    b = normpath(b).strip()
+    fn = b.split("/")[-1]
     data_type = 4
     if fn[0] == 'T':
         data_type = 4
@@ -32,7 +32,12 @@ for b in bins:
     else:
         pass
     if data_type == 6:
-        cmd = cmd = 'abs ' + str(nlin) + ' ' + str(ncol) + ' ' + b
-        if multi_look != 1:
-            cmd += ' ' + str(multi_look)
-        run(cmd)
+        run('abs ' + str(nlin) + ' ' + str(ncol) + ' ' + b)
+        abs_file = b + '.abs'
+        ml_file = abs_file + '.ml'
+        run('multilook ' + abs_file + ' ' + os.path.dirname(cwd) + ' ' + ml_file + ' ' + str(multi_look))
+        ht_file = ml_file + '.ht'
+        run('htrim ' + ml_file + ' ' + str(int(nlin/multi_look)) + ' ' + str(ncol) + ' 1.5 1.5 ' + ht_file)
+
+        
+
