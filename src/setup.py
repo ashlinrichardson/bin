be9ac98cd1d7d6789e6009f474a85ee1b69df9e0
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-08 21:06:56 -0700

    compile everything in the package...
    compile everything in the package...

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from fl0w3r import normpath, run
+src_dir = normpath(os.path.dirname(__file__))
+def files(ext):
+  return os.popen('find ' + src_dir + ' -name "*.' + ext + '"').readlines()
+
+pyfiles = files('py')
+cfiles = files('c')
+cppfiles = files('cpp')
+
+cmpl = src_dir + 'compile.py'
+
+for afiles in [pyfiles, cfiles, cppfiles]:
+  for f in afiles:
+    f = f.strip()
+    if(f.split('/')[-1] != '__init__.py'):
+      f = normpath(f)
+      run(cmpl + ' ' + f)
+      
+
+
