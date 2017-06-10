9f11d9090aca9db68bc04b611c0024c95d17e1e0
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-10 00:38:46 -0700

    Convert floating point type HSV binary files into integer type (scaled)
     Convert floating point type HSV binary files into integer type
    (scaled)

---
+++ src/btl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+from fl0w3r import error, normpath, run
+
+args = sys.argv
+if len(args) < 2:
+  error("btl.py: Convert floating point type HSV binary files into integer type (scaled) e.g., for use with Logit.\nNote that a radar config.txt file must be present.\nUsage: btl [directory path]\n")
+
+cwd = normpath(args[1])
+NRow, NCol = read_config(cwd + "config.txt")
+
+run("btlsf " + cwd + "Hscaled.bin " + str(NRow) + " " + str(NCol) + " " + cwd + "Hlogit.bin")
+run("btlsf " + cwd + "Sscaled.bin " + str(NRow) + " " + str(NCol) + " " + cwd + "Slogit.bin")
+run("btlsf " + cwd + "Vscaled.bin " + str(NRow) + " " + str(NCol) + " " + cwd + "Vlogit.bin")
