449bf3987c8bd4e8fc43eaa81d0eee19f2f1634f
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-11 16:01:50 -0700

    tidy up
    tidy up

--- src/T3.h
+++ src/T3.h
@@ -1,31 +1,10 @@
-//;***********************************************************************************************************
-//;*
-//;* T3.h
-//;* squall\palsar\pos\RADLIB\T3.h
-//;*
-//;***********************************************************************************************************
-//;*
-//;* This header performs library type functions for T3 matrix data. See:
-//;* squall/wiki/index.php/RADLIB
-//;*
-//;* Status: R&D
-//;*
-//;* Last modification:March 18, 2008.
-//;*
-//;* Developer: Ashlin Richardson <ashricha@nrcan.gc.ca> <ashlin.richardson@gmail.com>
-//;*
-//;* Modified by A. Richardson on March 18th, 2008 for commenting
-//;*
-//;*
-//;***********************************************************************************************************
-
 #pragma once
 #include <iostream>
 #include <ostream>
 #include <vector>
 #include "bin.h"
 
-// define indexes for each channel of information from T3 matrix
+/* define indexes for each channel of information from T3 matrix */
 #define  r11  0
 #define  i12  1
 #define  r12  2
@@ -42,83 +21,64 @@ using std::vector;
 using namespace _bin;
 
 namespace _T3{
-
-
-
 	class T3	{
-		public:
+	  public:
 
 			float pixel[nT3Files];
 
-			T3(int _type, char * _dir)
-			{
-				//printf("T3::T3\n");
-				//T = \0;
+			T3(int _type, char * _dir){
 				T = new Image[nT3Files];
 				for (i=0; i<nT3Files; i++) pixel[i]=0.0;
 
 				char file_name[100];
-			    	char * filenames[nT3Files] =
-				{"T11.bin",
-				"T12_imag.bin",
-				"T12_real.bin",
-				"T13_imag.bin",
-				"T13_real.bin",
-				"T22.bin",
-				"T23_imag.bin",
-				"T23_real.bin",
-				"T33.bin"
-					};
+			  char * filenames[nT3Files] = {"T11.bin", "T12_imag.bin", "T12_real.bin", "T13_imag.bin", "T13_real.bin", "T22.bin", "T23_imag.bin", "T23_real.bin", "T33.bin"};
 
-				NRow=NCol=0;
+				NRow = NCol = 0;
 				dir = _dir;
 				getT3_Image_Dimensions(dir, NRow, NCol);
 				type=_type;
 				printf("NRow %i NCol %i\n", NRow, NCol);
 
-
 				for (i=0; i<nT3Files; i++){
 					sprintf(file_name, "%s%s", dir, filenames[i]);
 					T[i].setDimensions(NRow, NCol);  //must call this before initImage
 					T[i].initImage(_type, file_name);
-
 					file_name[0]='\n';
 				}
 				return;
 			}
-			void setPixel()
-			{
-				for(i=0; i<nT3Files; i++)
-					T[i].setPixel(pixel[i]);
 
+			void setPixel(){
+				for(i=0; i<nT3Files; i++){
+					T[i].setPixel(pixel[i]);
+          }
 			}
-			void getPixel()
-			{
-				for(i=0; i<nT3Files; i++)
-				{
+
+			void getPixel(){
+				for(i=0; i<nT3Files; i++){
 					pixel[i]=T[i].getPixel();
 				}
 			}
-			void writeHeaders()
-			{
 
+			void writeHeaders(){
 			}
-			~T3()
-			{
+
+			~T3(){
 			}
-			void close()
-			{
+
+			void close(){
 				for(i=0; i<nT3Files; i++){
 					T[i].close();
 					T[i].destroy();
-
 				}
 			}
+
 			void getDimensions(int & _NRow, int & _NCol){
 				_NRow=NRow;
 				_NCol=NCol;
 
 			}
+
 			void rewind(){
 				int NR, NC;
 				for (i=0; i<nT3Files; i++){
@@ -127,14 +87,9 @@ namespace _T3{
 			}
 
 		protected:
-		int type;
 		char * dir;
 		Image * T;
-		int NRow, NCol;
-
-		private:
-		int i;
-
+		int NRow, NCol, type, i;
 	};
 };
 
