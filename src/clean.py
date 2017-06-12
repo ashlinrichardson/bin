f57f8488f71276e33cb2d3a6776a4e24c6745b23
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-11 20:59:30 -0700

    tidy up pesky swap files or object files
    tidy up pesky swap files or object files

---
+++ src/clean.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''clean up swap files, executables/object code, or other redundant files'''
+import os
+import sys
+from ansicolor import KRED, KGRN, KNRM
+to_re_move = ['swn', 'swo', 'swp', 'exe', 'gch', 'o', 'pickle', 'pyc', 'bak']
+
+
+def re_move(ext):
+    my_files = os.popen('find ./ -name "*.' + ext + '"').readlines()
+    for f in my_files:
+        fn = f.strip()
+        if os.path.exists(fn):
+            a = os.popen("rm "+fn).read()
+            print("\tdel " + KRED + fn + KNRM)
+
+for e in to_re_move:
+    re_move(e)
