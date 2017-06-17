50648cfaa38307978e8dd87b8713593cd8929d19
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-17 16:52:44 -0700

    reindent
    reindent

--- src/br.cpp
+++ src/br.cpp
@@ -5,31 +5,31 @@
 
 using namespace std;
 int main(int argc, char ** argv){
-	if(argc < 6){
-		error("br.cpp: calculate the ratio of two 32bit floating point channels.\n\nUsage: br [numerator] [denominator] [outfile] [nfloats]\n");
-	}
-
-	FILE * f = open(argv[1]);
-	FILE * g = open(argv[2]);
-	FILE * of = wopen(argv[3]);
-
-	int nr = atoi( argv[4]);
-	int nc = atoi(argv[5]);
-
-	int nf = nr * nc;
-	int i;
-
-	float F, G, C;
-
-	for(i=0; i<nf; i++){
-		fread(&F, sizeof(float), 1, f);
-		fread(&G, sizeof(float), 1, g);
-		C = F / G;
-		fwrite(&C, sizeof(float), 1, of);
-	}
-	fclose(f);
-	fclose(g);
-	fclose(of);
-
-	return(0);
+  if(argc < 6){
+    error("br.cpp: calculate the ratio of two 32bit floating point channels.\n\nUsage: br [numerator] [denominator] [outfile] [nfloats]\n");
+  }
+  
+  FILE * f = open(argv[1]);
+  FILE * g = open(argv[2]);
+  FILE * of = wopen(argv[3]);
+  
+  int nr = atoi( argv[4]);
+  int nc = atoi(argv[5]);
+  
+  int nf = nr * nc;
+  int i;
+  
+  float F, G, C;
+  
+  for(i=0; i<nf; i++){
+    fread(&F, sizeof(float), 1, f);
+    fread(&G, sizeof(float), 1, g);
+    C = F / G;
+    fwrite(&C, sizeof(float), 1, of);
+  }
+  fclose(f);
+  fclose(g);
+  fclose(of);
+  
+  return(0);
 }
