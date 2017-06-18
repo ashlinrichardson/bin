358474b05fa837e4f1f28ca867c6bce8a3bdc841
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-06-17 22:26:41 -0700

    Update band.h

--- src/band.h
+++ src/band.h
@@ -5,20 +5,14 @@
 #include <fstream>
 #define BIG_BUFFER (1024*1024)
 
-template <class T>
 /*! \brief Represents the data and functionality of one Band in a Band Sequential Image.
 * Brief description continued.
 *
 * Detailed description goes here.
 */
-class Band{
-
+template <class T> class Band{
   public:
-  /*! \brief A constructor
-  * ...
-  *
-  * ...
-  */
+
   Band(FILE* file, long samples, long lines, long bands, long header_offset, int data_type, int byte_order ){
     setFileHandle(file);
     setSamples(samples);
@@ -28,72 +22,47 @@ class Band{
     setDataType(data_type);
     setByteOrder(byte_order);
   }
+  
   ~Band();
 
-  /*! \brief Set the file handle from which BSQ data will be fetched.
-  * ...
-  *
-  * ...
-  */
   inline void setFileHandle(FILE * file){
+    /* set file handle to fetch BSQ data from */
     this->file=file;
-    if(file){
-      printf("Error: failed to open input file\n");
+    if(!file){
+      printf("Error: bad file handle\n");
       exit(1);
     }
   }
 
-  /*! \brief Set the number of Lines in the BSQ file.
-  * ...
-  *
-  * ...
-  */
   inline void setLines(long lines){
+    /* number of lines in BSQ file */
     this->lines=lines;
   }
 
-  /*! \brief Set the number of Samples in the BSQ file.
-  * ...
-  *
-  * ...
-  */
   inline void setSamples(long samples){
+    /* number of samples in BSQ file */
     this->samples=samples;
   }
 
-  /*! \brief Set the number of Bands in the BSQ file.
-  * ...
-  *
-  * ...
-  */
   inline void setBands(long bands){
+    /* number of bands in BSQ file */
     this->bands=bands;
   }
+  
   inline void setHeaderOffset(long header_offset){
     this->header_offset=header_offset;
-    //========================
-
-    //========================
   }
 
-  /*! \brief Set the type of file to be read.
-  * ...
-  *
-  * Currently the only file type supported is "ENVI" file type.
-  */
   inline void setFileType(char* file_type){
+    /* set filetype (currently only "ENVI" supported) */
     this->file_type=file_type;
     if( (file_type[0]!='E')||(file_type[1]!='N')||(file_type[2]!='V')||(file_type[3]!='I')){
       printf("Error: only ENVI file type is currently supported\n");
     }
   }
 
-  /*! \brief Set the type of data to be read.
-  * ...
-  *
-  * The data types being implemented are float (4) and long (3).
-  */
   inline void setDataType(int data_type){
+    /* set data type */
     this->data_type=data_type;
     if( !((data_type==3)||(data_type==4))){
       printf("Error: only data types 3 and 4 are supported\n");
@@ -101,12 +70,8 @@ class Band{
     }
   }
 
-  /*! \brief Set the type of interleave corresponding to the input file.
-  * ...
-  *
-  * The only interleave type currently being implemented is "bsq".
-  */
   inline void setInterleave(char * interleave){
+    /* set interleave (BSQ supported) */
     this->interleave = interleave;
 
     if((interleave[0]!='b')||(interleave[1]!='s')||(interleave[2]!='q')){
@@ -115,12 +80,8 @@ class Band{
     }
   }
 
-  /*! \brief Set the type of byte ordering corresponding to the input file.
-  * ...
-  *
-  * Both big- and little- -endian formats are being implemented.
-  */
   inline void setByteOrder(int byte_order){
+    /* set byte order */
     if(!((byte_order==0)||(byte_order==1))){
       printf("Error: byte order must be set to 0 or 1\n");
       exit(1);
@@ -128,15 +89,8 @@ class Band{
     this->byte_order = byte_order;
   }
 
-  /*! \brief Read the block of data corresponding to a given, zero-indexed band.
-  * ...
-  *
-  * The program is intended to read from an arbitrary band so that it is easy to implement a multi-threaded version of the app, with multiple Band objects declared, each representing the processing for a given band.
-  */
   inline void readBand(int band_index){
-    //========================
-
-    //========================
+    /* read a band */
   }
 
   private:
@@ -150,39 +104,18 @@ class Band{
   int byte_order;
 
 
-  /*! \brief The array to hold the data from one band of the BSQ image.
-  * ...
-  *
-  * Templates are used so that float, long, or other types may be easily used.
-  */
+/* array to hold a band */
   T ** data;
 
-  /*! \brief The array to hold the data to write to one band of the output BSQ image.
-  * ...
-  *
-  * ...
-  */
+/* array to hold an output band */
   T ** out_data;
 
-  /*! \brief An array to hold the data to write to an output file (of the same dimensions and data format) which is a mask representing which pixels have been changed by processing.
-  * ...
-  *
-  * 0 means that the given pixel was untouched, whereas 1 means the given pixel was touched.
-  */
+  /* array to hold the data to write to an output file (of the same dimensions and data format) which is a mask representing which pixels have been changed by processing. 0 means that the given pixel was untouched, whereas 1 means the given pixel was touched. */
   T ** mask;
 
-  /*! \brief The file handle of the image to be read.
-  * ...
-  *
-  *
-  */
+  /* The file handle of the image to be read. */
   FILE * file;
 
-  /*! \brief The file handle of the output image.
-  * ...
-  *
-  *
-  */
+  /* The file handle of the output image. */
   FILE * out_file;
-
 };
