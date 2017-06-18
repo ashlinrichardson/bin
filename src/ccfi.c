3b3df983b35783ffa4b8655cf29562fe0c52ba3c
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-06-17 22:42:49 -0700

    Update ccfi.c

--- src/ccfi.c
+++ src/ccfi.c
@@ -5,15 +5,12 @@ gcc ccfi.c -Wall -lgsl -lblas -lgslcblas
 using code from:
 
 http://www.gnu.org/software/gsl/manual/html_node/Root-Finding-Examples.html
-http://www.gnu.org/software/gsl/manual/html_node/Interpolation-Example-programs.html
+http://www.gnu.org/software/gsl/manual/html_node/Interpolation-Example-programs.html */
 
-*/
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
-#include <math.h>
-
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_math.h>
@@ -52,13 +49,13 @@ int main(int argc, char * argv[]){
   double dccf[nlines];
   double dlhq[nlines];
 
-  memset(&band[0], '\0', sizeof(float)*nlines);
-  memset(&ccf[0], '\0', sizeof(float)*nlines);
-  memset(&lhq[0], '\0', sizeof(float)*nlines);
+  memset(&band[0], '\0', sizeof(float) * nlines);
+  memset(&ccf[0], '\0', sizeof(float) * nlines);
+  memset(&lhq[0], '\0', sizeof(float) * nlines);
 
-  memset(&dband[0], '\0', sizeof(double)*nlines);
-  memset(&dccf[0], '\0', sizeof(double)*nlines);
-  memset(&dlhq[0], '\0', sizeof(double)*nlines);
+  memset(&dband[0], '\0', sizeof(double) * nlines);
+  memset(&dccf[0], '\0', sizeof(double) * nlines);
+  memset(&dlhq[0], '\0', sizeof(double) * nlines);
 
   char chars[MAXN];
   char * fn = argv[1];
@@ -67,13 +64,12 @@ int main(int argc, char * argv[]){
 
   FILE * f = fopen(fn, "rb");
 
-  int i=0;
-  for(i=0; i<4; i++){
+  int i = 0;
+  for(i = 0; i < 4; i++){
     fgets( &chars[0], MAXN, f);
   }
 
   int a = 0;
-
   i = 0;
 
   while((a = fscanf(f, "%f %f %f", &(band[i]), &(ccf[i]), &(lhq[i]))) & (a != -1)){
@@ -83,8 +79,8 @@ int main(int argc, char * argv[]){
     i++;
   }
 
-  for(i=0; i<(nlines-1); i++){
-    if((dband[i + 1] -dband[i]) < 0.){
+  for(i = 0; i < (nlines - 1); i++){
+    if((dband[i + 1] - dband[i]) < 0.){
       printf( "%i of %i Warning: %f %f %f\n", i, nlines, dband[i + 1], dband[i], dband[i + 1] - dband[i]);
       printf("ABORTED.\n");
       exit(1);
@@ -94,14 +90,17 @@ int main(int argc, char * argv[]){
   gsl_interp_accel * acc_ccf = gsl_interp_accel_alloc();
   gsl_spline * spline_ccf = gsl_spline_alloc(gsl_interp_cspline, nlines);
   gsl_spline_init (spline_ccf, dband, dccf, nlines);
+  
   gsl_interp_accel * acc_lhq = gsl_interp_accel_alloc();
   gsl_spline * spline_lhq = gsl_spline_alloc(gsl_interp_cspline, nlines);
   gsl_spline_init(spline_lhq, dband, dlhq, nlines);
 
   int status;
   int iter = 0, max_iter = 100;
+  
   const gsl_root_fsolver_type *T;
   gsl_root_fsolver *s;
+  
   double r = 0, r_expected = sqrt(lhqin);
   double x_lo = dband[0], x_hi = dband[nlines - 1];
   gsl_function F;
@@ -130,8 +129,9 @@ int main(int argc, char * argv[]){
     x_hi = gsl_root_fsolver_x_upper(s);
     status = gsl_root_test_interval(x_lo, x_hi, 0, 0.001);
 
-    if (status == GSL_SUCCESS) printf ("Converged:\n");
-
+    if (status == GSL_SUCCESS){
+        printf ("Converged:\n");
+    }
     printf ("%5d [%.7f, %.7f] %.7f %+.7f %.7f\n", iter, x_lo, x_hi, r, r - r_expected, x_hi - x_lo);
   }
   while(status == GSL_CONTINUE && iter < max_iter);
@@ -142,9 +142,10 @@ int main(int argc, char * argv[]){
     printf("ABORTED.\n");
     exit(1);
   }
-  printf("Interpolated value of LHQ for percentile height %f matched input LHQ: %f\n", r, iv );
+  printf("Interpolated value of LHQ for percentile height %f matched input LHQ: %f\n", r, iv);
 
   double CCF = gsl_spline_eval(spline_ccf, r, acc_ccf);
+  
   printf("Interpolated value of CCF for percentile height: %f\n", CCF );
   printf("PERCENTILE HEIGHT LHQ CCF\n");
   printf("%f %f %f\n", r, iv, CCF);
