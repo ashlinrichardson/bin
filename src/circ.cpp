bd538eed22c4ec903f93e8a9c856c27331b133aa
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-06-17 22:46:26 -0700

    Update circ.cpp

--- src/circ.cpp
+++ src/circ.cpp
@@ -1,17 +1,17 @@
 /* Generate circular polarization channels from scattering matrix. refs:
   [1] "S.H. Bickel, Some invariant properties of the polarization scattering matrix", 1965
   [2] Dr. Pottier's book */
+
 #include "radlib.h"
 using namespace _matrix2;
 
 #define nS2Files 4
 int main(int argc, char *argv[]){
-
-	if(argc!=2){
+	if(argc != 2){
 		printf("circ.cpp: generate circular polarization images LL LR RL RR from S11, S12, S21, S22.\n");
 		printf("Implemented by Ash Richardson, June 4, 2009 with revisions 20170610.\n");
-    printf("\tUsage: circ [in_dir]\nNote: config.txt file must be present in input directory\n");	
-	  exit(1);
+    			printf("\tUsage: circ [in_dir]\nNote: config.txt file must be present in input directory\n");	
+	  	exit(1);
 	}
 
 	int NRow, NCol, Row, Col;
@@ -22,7 +22,12 @@ int main(int argc, char *argv[]){
   S2_in.getDimensions(NRow, NCol);
 
   /* output data set */
-	char * outfilenames[nS2Files] = {"LL.bin", "LR.bin", "RL.bin", "RR.bin"};
+	char * outfilenames[nS2Files] = {
+		"LL.bin", 
+		"LR.bin", 
+		"RL.bin", 
+		"RR.bin"};
+	
 	S2 S2_out(OUTPUT, input_dir, outfilenames);
 	S2_out.setDimensions(NRow, NCol);
 	
