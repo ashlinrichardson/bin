a469d52642dfcd7d097541e496c1bf5cfa22bb73
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-19 14:10:48 -0700

    pep8
    pep8

--- src/message.py
+++ src/message.py
@@ -3,9 +3,10 @@ import smtplib
 from fl0w3r import error, args
 from ansicolor import KRED, KGRN
 
+
 def message(fromaddr, toaddrs, msg):
     print KRED + "FROM: " + KGRN + fromaddr
-    print KRED + " TO:" + KGRN + toaddrs 
+    print KRED + " TO:" + KGRN + toaddrs
     print KRED + " MSG: " + KGRN + str(msg).strip()
     ''' send an email with body "msg" '''
     msg = 'From: ' + fromaddr + '\r\nTo: ' + toaddrs + '\r\n\r\n' + msg
@@ -14,8 +15,10 @@ def message(fromaddr, toaddrs, msg):
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
 
+
 def prompt(prompt):
     return raw_input(prompt).strip()
+
 
 if __name__ == '__main__':
     fromaddr, toaddrs, msg = None, None, None
@@ -25,7 +28,7 @@ if __name__ == '__main__':
         msg = ' '.join(args[3:])
     else:
         fromaddr = prompt("From: ")
-        toaddrs  = prompt("To: ") # .split()
+        toaddrs = prompt("To: ")
         print "Enter message, end with ^D (Unix) or ^Z (Windows):"
         msg = ""
         while 1:
