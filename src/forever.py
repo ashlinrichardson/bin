a8563607bed21f61e513a8ed89dd6058964de6fe
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-19 11:01:39 -0700

    repeat a command at regular intervals
    repeat a command at regular intervals

---
+++ src/forever.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''repeat command at regular interval (in seconds): default 10 seconds
+if first command line argument can be interpreted as integer, override
+the default value. use remaining args as command to run. 20170619 AR'''
+import os
+import sys
+import time
+from fl0w3r import args, error
+
+default = 10
+delay, arg = default, args[1:]
+
+if len(args) > 1:
+    try:
+        delay = int(args[1])
+        arg = args[2:]
+    except:
+        pass
+
+cmd = ' '.join(args[(1 if delay == default else 2):])
+print cmd
+sys.exit(1)
+
+while(True):
+    a = os.system(cmd)
+    time.sleep(delay)
