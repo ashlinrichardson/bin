98aafb88ddfaed5148698a9e8ef73ab519cc9dd9
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-22 12:07:52 -0700

    bugfix
    bugfix

--- src/edit.py
+++ src/edit.py
@@ -6,7 +6,7 @@ priority. should add auto-commit'''
 import os
 import sys
 from copy import deepcopy
-from fl0w3r import run, error, normpath, args
+from fl0w3r import run, error, normpath, args, exists
 
 pwd = normpath(os.getcwd())
 home = normpath(os.path.expanduser('~/GitHub/fl0w3r/src/'))
@@ -23,28 +23,29 @@ suppor.append('h')
 ext = fn.split('.')[-1].strip()
 base = fn.split('/')[-1].strip()
 
-# embellish filename (if already exists)
-cmd = 'find ./ -name "' + base + '.*"'
-friends = os.popen(cmd).readlines()
-if len(friends) == 1:
-    fn = normpath(friends[0]).strip()
-    base = fn.split('/')[-1].strip()
-    ext = base.split('.')[-1].strip()
-else:
-    base = {f.split('/')[-1].strip(): f for f in friends}
-    exts = {b.split('.')[-1].strip(): base[b] for b in base.keys()}
-
-    # should compose dicts instead:
-    if 'py' in exts:
-        fn, ext = exts['py'], 'py'
-    elif 'cpp' in exts:
-        fn, ext = exts['cpp'], 'cpp'
-    elif 'c' in exts:
-        fn, ext = exts['c'], 'c'
-    elif 'h' in exts:
-        fn, ext = exts['h'], 'h'
+if not exists(fn):
+    # embellish filename (if already exists)
+    cmd = 'find ./ -name "' + base + '.*"'
+    friends = os.popen(cmd).readlines()
+    if len(friends) == 1:
+        fn = normpath(friends[0]).strip()
+        base = fn.split('/')[-1].strip()
+        ext = base.split('.')[-1].strip()
     else:
-        error('supported extensions: ' + (' '.join(suppor)))
+        base = {f.split('/')[-1].strip(): f for f in friends}
+        exts = {b.split('.')[-1].strip(): base[b] for b in base.keys()}
+    
+        # should compose dicts instead:
+        if 'py' in exts:
+            fn, ext = exts['py'], 'py'
+        elif 'cpp' in exts:
+            fn, ext = exts['cpp'], 'cpp'
+        elif 'c' in exts:
+            fn, ext = exts['c'], 'c'
+        elif 'h' in exts:
+            fn, ext = exts['h'], 'h'
+        else:
+            error('supported extensions: ' + (' '.join(suppor)))
 
 if not(ext in suppor):
     error('extension not supported: ' + str(ext))
