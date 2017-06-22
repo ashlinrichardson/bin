dede833a10fa435e35194d47c500ba79c512c6dd
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-22 10:28:37 -0700

    added searching for files, based on language-level
    added searching for files, based on language-level

--- src/edit.py
+++ src/edit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 '''workflow: open a file in vim: if not a header file, "compile".
-by ashlin richardson 20170621'''
+by ashlin richardson 20170621.
+20170622 added searching for files, based on language-level
+priority. should add auto-commit'''
 import os
 import sys
 from copy import deepcopy
@@ -17,9 +19,36 @@ if len(args) < 2:
 fn, compil = normpath(args[1]), ['py', 'c', 'cpp']
 suppor = deepcopy(compil)
 suppor.append('h')
-ext = fn.split('.')[-1]
+
+ext = fn.split('.')[-1].strip()
+base = fn.split('/')[-1].strip()
+
+# embellish filename (if already exists)
+cmd = 'find ./ -name "' + base + '*"'
+friends = os.popen(cmd).readlines()
+if len(friends) == 1:
+    fn = normpath(friends[0]).strip()
+    base = fn.split('/')[-1].strip()
+    ext = base.split('.')[-1].strip()
+else:
+    base = {f.split('/')[-1].strip() : f for f in friends}
+    exts = {b.split('.')[-1].strip() : base[b] for b in base.keys()}
+
+    print "base", base, "exts", exts
+    # should compose dicts instead:
+    if 'py' in exts:
+        fn, ext = exts['py'], 'py'
+    elif 'cpp' in exts:
+        fn, ext = exts['cpp'], 'cpp'
+    elif 'c' in exts:
+        fn, ext = exts['c'], 'c'
+    elif 'h' in exts:
+        fn, ext = exts['h'], 'h'
+    else:
+        error('supported extensions: ' + (' '.join(suppor)))
 
 if not(ext in suppor):
+    print "ext", ext, "suppor", suppor
     print 'edit.py: error: file not supported: ' + fn
     error('extension not supported: ' + str(ext))
 
