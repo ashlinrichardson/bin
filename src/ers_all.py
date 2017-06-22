32566842d36f7618dcc02d7ba6011cb6aef80206
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-22 11:52:02 -0700

    extract rs-2 data from a series of folders
    extract rs-2 data from a series of folders

---
+++ src/ers_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''run ers on a bunch of folders'''
+import os
+import sys
+from fl0w3r import run
+
+ls = os.popen('ls -1 *.zip').readlines()
+pwd = os.popen('pwd').read().strip()
+
+for l in ls:
+	fn = l.strip();
+	if((fn[:3] == 'RS2') & (fn[-4:] == '.zip')):
+		rs2folder = pwd + "/" + fn[:-4] + "/"
+		print rs2folder
+		run("ers " + rs2folder + " " + rs2folder)
