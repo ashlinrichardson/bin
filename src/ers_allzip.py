cd434c4ea1f9d914451b61fb80bfe2fc33a468a7
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-22 11:52:06 -0700

    extract rs-2 data from a series of folders
    extract rs-2 data from a series of folders

---
+++ src/ers_allzip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+from fl0w3r import run
+print("Extracting all zip files in current directory...")
+lines = os.popen('find ./ -name "RS2*SLC"').readlines()
+cd = os.getcwd()
+print("cd " + cd)
+
+for line in lines:
+	  f = os.path.abspath(line.strip())
+	  os.chdir(f)
+	  run("ers ./ ./")
+	  os.chdir(cd)
