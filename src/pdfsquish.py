921f179ba9b088bde5b950a5b6b19394ab4890e1
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-26 13:26:24 -0700

    reduce pdf file size
    reduce pdf file size

---
+++ src/pdfsquish.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+import sys
+from fl0w3r import args
+
+if len(args) < 3:
+    error("pdfsquish.py.\nUsage: [infile.pdf] [outfile.pdf]" +
+          "\nReduce pdf file size to screen resolution")
+
+infile, outfile = normpath(args[1]), normpath(args[2])
+
+if not os.path.exists(infile):
+    error("input file not found" + str(infile))
+
+run("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen " +
+    "-dNOPAUSE -dQUIET -dBATCH -sOutputFile=" + outfile + " " + infile)
