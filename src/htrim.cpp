e7dc659b9fc97cc8121d01c9d202d75b8889fbd5
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-25 22:54:00 -0700

    histogram trimming of binary files
    histogram trimming of binary files

--- src/htrim.cpp
+++ src/htrim.cpp
@@ -1,115 +1,155 @@
+/*
+#----------------------------------------------------
+# file: htrim/htrim.cpp
+#
+# description: histogram trimming on type-4 envi
+# format data
+#
+# date: march 30, 2012. reimpl 20170625
+# author: ashlin richardson (ashy@uvic.ca)
+#----------------------------------------------------
+*/
+
 #include <limits.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+#include <list>
 #include <float.h>
 #include <time.h>
-#include "envi.h"
-#include <list>
+#include"envi.h"
+
+#define ENVI_FLOAT_TYPE 4
 
 int main(int argc, char ** argv){
-  
+
+  char in_dir[1000];
+
   if(argc<6){
-    printf("htrim.cpp: trim a binary file (ENVI type 4) by histogram modification. Trim a percentage from the top and bottom.\n");
-    printf("reimplemented 20170508 from 20120330 version\n");
-    printf("Usage: trim [infile] [nrow] [ncol] [lower %] [upper %] [outfile]\n");
+    printf("trim.cpp: trim a binary file by histogram modification.");
+    printf("(reimpl. 20170625) Trim a percentage from the top and bottom.\n");
+    printf("Usage: trim [in_dir] [infile] [lower %] [upper %] [outfile]\n");
     exit(1);
   }
-  
-  char * infile = argv[1];
-  int Nrow = atoi(argv[2]);
-  int Ncol = atoi(argv[3]);
-  float p1 = atof(argv[4]);
-  float p2 = atof(argv[5]);
-  char * outfile = argv[6];
-  int index_lower = int(floor((p1 / 100.) * ((float)Nrow) * ((float)Ncol)));
-  int index_upper = int(floor(((100. - p2) / 100.) * ((float)Nrow) * ((float)Ncol)));
 
-  printf("index lower %d index upper %d Nrow*Ncol %d\n", index_lower, index_upper, Nrow * Ncol);
+  strcpy(in_dir, argv[1]);
+  char * infile = argv[2];
 
-  FILE * outf = wopen(outfile);
-  FILE * inf = open(infile);
-  float min, max, dat;
-  int lig, col, count;
-  std::list<float> my_list;
-  min = max = FLT_MAX;
-  count = 0;
-  
-  for(lig = 0; lig < Nrow; lig++){
+  char PolarCase[20];
+  char PolarType[20];
+  int Nlig, Ncol, Np;
+  float p1 = atof(argv[3]);
+  float p2 = atof(argv[4]);
+  char * outfile = argv[5];
+
+  read_config(in_dir, Nlig, Ncol);
+  int NRow, NCol;
+  NRow = Nlig;
+  NCol = Ncol;
+
+  int index_lower = int( floor( (p1 / 100.) * (float)NRow * (float)NCol));
+  int index_upper = int( floor( ( (100.-p2) / 100.) * (float)NRow * (float)NCol));
+
+  printf("index lower %d\n", index_lower);
+  printf("index upper %d\n", index_upper);
+  printf("Nrow*NCol %d\n", NRow*NCol);
+
+  write_envi_hdr(infile, Nlig, Ncol, ENVI_FLOAT_TYPE);
+  write_envi_hdr(outfile, Nlig, Ncol, ENVI_FLOAT_TYPE);
+
+  FILE * outf = fopen( outfile, "wb");
+
+  if (!outf){
+
+    printf("Could not open output file\n");
+    exit(1);
+  }
+
+  FILE * inf = fopen( infile, "rb");
+  if( !inf){
+    printf("Could not open input file.\n");
+    exit(1);
+  }
+
+  float min, max;
+
+  min = FLT_MAX;
+  max = FLT_MIN;
+
+  std::list<float> mylist;
+
+  float dat;
+
+  int lig, col;
+
+  int count =0;
+  for(lig = 0; lig < Nlig; lig++){
     for(col = 0; col < Ncol; col++){
-      count++;
+      count ++;
       fread(&dat, sizeof(float), 1, inf);
-      my_list.push_back(dat);
-      
-      if((!isinf(dat)) && (!isnan(dat))){
-        if(min > dat){
-          min = dat;
-        }
-        if(max < dat){
-          max = dat;
-        }
+      mylist.push_back(dat);
+
+      if((!isinf( dat)) && (!isnan(dat))){
+        if(min > dat) min = dat;
+        if(max < dat) max = dat;
       }
-      
+
     }
   }
-  printf("Min %e Max %e\n", min, max);
+  printf("Min %e\n", min);
+  printf("Max %e\n", max);
 
-  /* another pass on the file coming */
-  rewind(inf);
+  fclose(inf);
 
-  /* sort the list of values */
-  my_list.sort();
+  inf = fopen(infile, "rb");
 
-  for(int q = 0; q < index_lower; q++){
-    my_list.pop_front();
+  mylist.sort();
+  int q;
+  for(q = 0; q < index_lower; q++){
+    mylist.pop_front();
   }
-  
-  float lower_cut = my_list.front();
-  index_upper = (Nrow * Ncol) - index_upper - 1;
-  
-  for(int q = 0; q < index_upper; q++){
-    my_list.pop_back();
+  float lowercut = (mylist.front()); //back());
+
+  index_upper = (NRow * NCol) - index_upper -1;
+  for(q = 0; q < index_upper; q++){
+    mylist.pop_back();
   }
-  
-  float top_cut = my_list.back();
+  float topcut = mylist.back();
 
-  printf("index_lower %d index_upper %d nrow*ncol %d\n", index_lower, index_upper, Nrow * Ncol);
-  printf("Lowercut %e Topcut %e\n", lower_cut, top_cut);
+  printf("Index lower %d Index upper %d\n", index_lower, index_upper);
 
-  if(isnan(lower_cut) || isinf(lower_cut)){
-    lower_cut = min;
-    printf("Warning: NewLower_cut %e topcut %e\n", lower_cut, top_cut);
-  }
+  printf("Bot %e Top %e\n", lowercut, topcut);
+  printf("Lowercut %e Topcut %e\n", lowercut, topcut);
 
-  if(isnan(top_cut) || isinf(top_cut)){
-    top_cut = max;
-    printf("Warning: lowercut %e NewTopcut %e\n", lower_cut, top_cut);
+  if(isnan(lowercut) || isinf(lowercut)){
+    lowercut = min;
+    printf("Newlowercut %e Topcut %e\n", lowercut, topcut);
   }
 
-  printf("Applying histogram derived cutoffs..\n");
-  for(lig = 0; lig < Nrow; lig++){
+  if(isnan(topcut) || isinf(topcut)){
+    topcut = max;
+    printf("Lowercut %e NewTopcut %e\n", lowercut, topcut);
+  }
+
+  printf("Applying histogram derived cutoffs to Hue and Value...\n");
+
+  for(lig = 0; lig < Nlig; lig++){
     for(col = 0; col < Ncol; col++){
-      
       fread(&dat, sizeof(float), 1, inf);
 
-      if(dat < lower_cut){
+      if(dat < lowercut){
         dat = 0.;
       }
-      else if(dat > top_cut){
+      else if(dat > topcut){
         dat = 1.;
       }
       else{
-        dat = (dat - lower_cut) / (top_cut - lower_cut);
+        dat = (dat - lowercut) / (topcut-lowercut);
       }
-      
       fwrite(&dat, sizeof(float), 1, outf);
-      
     }
   }
-
-  write_envi_hdr(string(outfile) + string(".hdr"), Nrow, Ncol);
-  printf("done");
   fclose(inf);
   fclose(outf);
 }
