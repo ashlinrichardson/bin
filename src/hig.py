36fbca2cf1a7bd97d1ae041bb3538d8a2cb61c9b
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2017-06-25 19:37:59 -0700

    Update hig.py

--- src/hig.py
+++ src/hig.py
@@ -2,10 +2,10 @@
 '''visualize multi-band image as interlaced gif 20170625'''
 import os
 import sys
-import matplotlib.image as mpimg
 import math
 import array
 from envi import read_envi_hdr
+import matplotlib.image as mpimg
 from fl0w3r import run, normpath, exists, error, args, chkfile, wopen
 
 
@@ -17,18 +17,22 @@ if len(args) < 2:
           "By Ash Richardson, May 5, 2009." +
           "\tUsage: hi [ENVI format hyperspectral image (data type 4)]")
 
+    
 fn = normpath(args[1])
 hfn = fn + ".hdr"
 chkfile(fn)
 chkfile(hfn)
 
+
 # read image dimensions from header file
 lines, samples, bands = read_envi_hdr(hfn)
 lines, samples, bands = int(lines), int(samples), int(bands)
 
+
 data = array.array('f')
 data.fromfile(open(fn), samples * lines * bands)
 npx = samples * lines
+
 for b in range(0, bands):
     band = data[b * npx: (b + 1) * npx]
     mn, mx = band[0], band[0]
@@ -41,9 +45,11 @@ for b in range(0, bands):
             mn = di if di < mn else mn
             mx = di if di > mx else mx
     print "band", b, "min", mn, "max", mx
+
     for i in range(0, len(band)):
         band[i] = 255. * (band[i] - mn) / (mx - mn)
         band[i] = band[i] if not bad(band[i]) else 0.
+    
     fn = 'jpg' + str(b).zfill(5) + '.jpg'
     dat, ci = [], 0
     for i in range(0, lines):
