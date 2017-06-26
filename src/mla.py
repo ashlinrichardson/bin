c2c0804aed89b706961e32ef0755ef3b5b9ce5ee
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-26 13:03:13 -0700

    multilink all files in dir
    multilink all files in dir

---
+++ src/mla.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+from fl0w3r import args, error, normpath
+
+if len(args) < 4:
+    error("mla.py: multilook all binary files in directory." +
+          "Usage: mla [input path] [output path] [window size]")
+
+inp, out = normpath(args[1]), normpath(args[2])
+win = int(args[3])
+
+result = os.popen("find "+inp+" -name \"*.bin\"").read()
+'''Usage: multilook [input file] [path to directory containing config.txt file]
+[output file] [window size]'''
+
+result = result.strip().split()
+
+for infile in result:
+    fn = normpath(infile)
+    run("multilook " + infile + " " + inp + "  " + out + " " + fn + " " +
+        str(win))
