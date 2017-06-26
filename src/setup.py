d8151480f7cc7c1156c9c3e00c1eea63628cbcca
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-26 10:34:33 -0700

    add gsl support for ubuntu
    add gsl support for ubuntu

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,14 @@ import os
 import sys
 from fl0w3r import normpath, run
 src_dir = normpath(os.path.dirname(__file__))
+
+# install gsl on Ubuntu machine (still need to implement this for others)
+if os.popen('uname -a').read().strip().split()[0] == 'Ubuntu':
+    run('sudo apt-get install libgsl0-dev')
+
+
 def files(ext):
-  return os.popen('find ' + src_dir + ' -name "*.' + ext + '"').readlines()
+    return os.popen('find ' + src_dir + ' -name "*.' + ext + '"').readlines()
 
 pyfiles = files('py')
 cfiles = files('c')
@@ -14,8 +20,8 @@ cppfiles = files('cpp')
 cmpl = src_dir + 'compile.py'
 
 for afiles in [pyfiles, cfiles, cppfiles]:
-  for f in afiles:
-    f = f.strip()
-    if(f.split('/')[-1] != '__init__.py'):
-      f = normpath(f)
-      run(cmpl + ' ' + f)
+    for f in afiles:
+        f = f.strip()
+        if(f.split('/')[-1] != '__init__.py'):
+            f = normpath(f)
+            run(cmpl + ' ' + f)
