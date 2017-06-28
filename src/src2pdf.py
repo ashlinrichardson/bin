83c61c355ef5b674a1912b0b954060a4c4e6272a
ashlinrichardson <richardson.ashlin@gmail.com>
2017-06-28 12:10:28 -0700

    create a pdf of source code (from VIM) with syntax colouring
    create a pdf of source code (from VIM) with syntax colouring

---
+++ src/src2pdf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+from fl0w3r import run, error, exists, args
+
+if len(args) < 2:
+    error('usage: src2pdf [source code file name]')
+
+fn = args[1]
+fnbase = (os.path.splitext(os.path.abspath(fn)))[0]
+
+run('vim ' + fnbase + '.ps -c ":hardcopy > '   '" -c ":q"')
+run('ps2pdf ' + fnbase + '.ps ' + fnbase + '.pdf')
