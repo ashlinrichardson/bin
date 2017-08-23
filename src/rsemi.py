75dc5da680656058b1a4ea5f1d530f42c22742b6
ashlinrichardson <richardson.ashlin@gmail.com>
2017-08-22 22:00:53 -0700

    remove right-semicolons from .py file
    remove right-semicolons from .py file. Written on a BC ferry from
    Vancouver to Victoria

---
+++ src/rsemi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/python
+'''rsemi.py: remove right semicolons from e.g., python files
+by ashlin richardson 20170822'''
+from fl0w3r import args, error, exists, wopen
+
+if len(args) < 2:
+    error('rsemi.py: usage: rsemi [input file.py]')
+
+fn = args[1]
+if not exists(fn):
+    error('rsemi.py: file not found: ' + str(fn.strip()))
+if fn[-2:] != 'py':
+    error('rsemi.py: intended for python code')
+
+tf = fn + '.bak'
+dat = open(fn).read()
+wopen(tf).write(dat)
+lines = dat.split('\n')
+
+for i in range(0, len(lines)):
+    lines[i] = lines[i].rstrip(';')
+
+wopen(fn).write('\n'.join(lines))
