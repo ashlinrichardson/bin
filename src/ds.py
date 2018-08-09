560995ae39ed220ba28d4ec96318787a311d75b8
ashlinrichardson <richardson.ashlin@gmail.com>
2018-08-08 23:38:50 -0700

    "size" of folder (replacement for "du")
    "size" of folder (replacement for "du")

---
+++ src/ds.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+''' ds.py: count number of bytes in present directory'''
+import os
+import sys
+from fl0w3r import error, normpath
+
+ds, dd = 0, 0
+for root, dirs, files in os.walk(os.getcwd()):
+        root = normpath(root)
+        # if os.path.isdir(root):
+
+        for f in files:
+            # print root + f
+            ds += os.stat(root + f).st_size
+
+        for d in dirs:
+            print root + d
+            dd += os.stat(root + d).st_size
+
+print "files bytes", ds
+print "direc bytes", dd
+print "total bytes", ds + dd
