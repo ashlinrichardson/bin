05b49a741f1c88338fb100607275db797bc17567
Ashlin Richardson <ashlinrichardson@users.noreply.github.com>
2019-05-11 14:17:33 -0700

    Update sbo.cpp

--- src/sbo.cpp
+++ src/sbo.cpp
@@ -30,7 +30,7 @@ int main(int argc, char ** argv){
     nb ++;
     d.push(c);
 
-    if(d.size() == n){
+    if(d.size() >= n){
 	    // write out one record
       while(d.size() > 0){
 	c = d.top();
