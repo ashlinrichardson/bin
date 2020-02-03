f0472a8a8c64475e7c98984b53b7f6610e50cf43
ashlinrichardson <richardson.ashlin@gmail.com>
2020-02-03 12:19:50 -0800

    blacklist nouveau driver

---
+++ src/ubuntu_blacklist_nouveau.py
@@ -0,0 +1,13 @@
+'''"blacklist" default graphics driver on ubuntu 18
+
+use this to install graphics drivers, e.g. for using any graphics hardware for any purpose incl. GPGPU
+
+https://linuxconfig.org/how-to-disable-nouveau-nvidia-driver-on-ubuntu-18-04-bionic-beaver-linux
+'''
+from flow3r import err, run
+run('sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"')
+run('sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"')
+run('cat /etc/modprobe.d/blacklist-nvidia-nouveau.conf')
+run('sudo update-initramfs -u')
+run('sudo reboot')
+
