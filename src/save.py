7583e16cbf0574f25c29377e673696f0efbb52e5
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-22 15:33:23 -0700

    update

--- src/save.py
+++ src/save.py
@@ -10,12 +10,14 @@ def run(c):
     a = os.system(c)
     if a != 0: err("command failed: [" + c + "]")
 
-if len(args) < 2: err("Usage: save [file name]")
-fn = args[1]
+if len(args) < 2:
+    err("Usage: save [file name]")
 
-if not os.path.exists(fn):
-    err("Error: file not found: " + fn)
+fns = args[1:]
+for fn in fns:
+    if not os.path.exists(fn):
+        err("Error: file not found: " + fn)
+    run('git add ' + fn)
 
-run('git add ' + fn)
 run('git commit -m "update"')
 run('git push origin master')
