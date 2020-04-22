2d99a34a7a9705684a0593774f1720f6accac4d1
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-22 14:46:14 -0700

    update

---
+++ src/save.py
@@ -0,0 +1,21 @@
+import os
+import sys
+args = sys.argv
+
+def err(m):
+    print("Error: " + str(m)); sys.exit(1)
+
+def run(c):
+    print(c)
+    a = os.system(c)
+    if a != 0: err("command failed: [" + c + "]")
+
+if len(args) < 2: err("Usage: save [file name]")
+fn = args[1]
+
+if not os.path.exists(fn):
+    err("Error: file not found: " + fn)
+
+run('git add ' + fn)
+run('git commit -m "update"')
+run('git push origin master')
