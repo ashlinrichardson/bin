ca4748ccceb30c34395b855ecb49e31e4028eb38
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-23 13:10:22 -0700

    update

--- src/setup.py
+++ src/setup.py
@@ -9,13 +9,27 @@ src_dir = normpath(os.path.dirname(__file__))
 if os.popen('uname -a').read().strip().split()[0] == 'Ubuntu':
     run('sudo apt-get install libgsl0-dev')
 
+pyfiles, cfiles, cppfiles = [], [], []
 
 def files(ext):
     return os.popen('find ' + src_dir + ' -name "*.' + ext + '"').readlines()
 
-pyfiles = files('py')
-cfiles = files('c')
-cppfiles = files('cpp')
+if len(sys.argv) < 2:
+    pyfiles = files('py')
+    cfiles = files('c')
+    cppfiles = files('cpp')
+else:
+    f = sys.argv[1]
+    ext = f.split('.')[-1]
+    if ext == 'py':
+        pyfiles = [f]
+    elif ext =='c':
+        cfiles = [f]
+    elif ext =='cpp':
+        cppfiles = [f]
+    else:
+        print("Error: unsupported file type: " + f)
+        sys.exit(1)
 
 cmpl = src_dir + 'compile.py'
 
