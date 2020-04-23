2e91e001ac749a47bfa8804575004031070808a1
ashlinrichardson <richardson.ashlin@gmail.com>
2020-04-23 13:02:46 -0700

    revise

--- src/save.py
+++ src/save.py
@@ -1,23 +1,25 @@
 import os
 import sys
 args = sys.argv
+msg = "update"
 
 def err(m):
     print("Error: " + str(m)); sys.exit(1)
 
 def run(c):
-    print(c)
+    print('[' + c + ']')
     a = os.system(c)
     if a != 0: err("command failed: [" + c + "]")
 
 if len(args) < 2:
-    err("Usage: save [file name]")
+    err("Usage: save [file name] # add an extra parameter that's not a file, to change the commit msg")
 
 fns = args[1:]
 for fn in fns:
     if not os.path.exists(fn):
-        err("Error: file not found: " + fn)
+        # err("Error: file not found: " + fn)
+        msg = fn.replace('"', ' ')
     run('git add ' + fn)
 
-run('git commit -m "update"')
+run('git commit -m "'  + msg + '"')
 run('git push origin master')
