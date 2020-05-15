9316513f6a856128d5c2faeda2b17675bc9e383c
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-05-14 18:29:29 -0700

    update

--- src/compile.py
+++ src/compile.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''compile scripts: this script expects to live at relative path:
       src/compile.py'''
 import os
@@ -84,7 +84,7 @@ if exists(binname):
     a = os.system(cmd)
 
 command_args, dat = '', None
-shebang = "#!/usr/bin/env python\n"
+shebang = "#!/usr/bin/env python3\n"
 comp_m = KGRN + "compile time: " + KRED + str(time_string) + KNRM
 last_comp = "print('" + comp_m + "')\n"
 imp = "import sys\nsys.path.append('" + source_folder + "')\n"
