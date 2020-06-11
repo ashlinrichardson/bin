522033ba4dc8d40a97d8fe98f750e39929158052
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-06-11 15:06:06 -0700

    update

---
+++ src/png2pdf.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def run(c):
+    print(c)
+    if os.system(c) != 0:
+        print("Error: command failed: " + c)
+        sys.exit(1)
+
+png = os.popen("ls -1 *.png").readlines()
+
+pns = []
+for p in png:
+    pn = p + ".pdf"
+    pns.append(pn)
+    run("convert " + p + " " + pn)
+
+if len(pns) > 0:
+    run("pdfcat " + " ".join(pns) + " out.pdf")
+
+
+
