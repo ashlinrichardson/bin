5f119206bf51a298df112731e5d70b72f6393a5e
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-07-09 15:16:26 -0700

    update

--- src/modef.cpp
+++ src/modef.cpp
@@ -77,9 +77,9 @@ int main(int argc, char ** argv){
   double fh = 0;
 
   for(row = 0; row < NRow; row++){
-    if(row % 10 == 0){
+    // if(row % 10 == 0)
       printf("\rProcessing row %d of %d", row+1, NRow);
-    }
+    
 
     for(col = 0; col < NCol; col++){
       totaln = 0;
