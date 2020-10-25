f4f9c677522ee12b5fe951d1e8f7022600849db1
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-24 22:25:07 -0700

    update

---
+++ src/scm_test/clean.sh
@@ -0,0 +1,5 @@
+rm -rf fra.bin
+rm -rf logfile.bak
+rm -rf RS2_OK76385_PK678063_DK606752_FQ2_20080415_143807_HH_VV_HV_VH_SLC
+rm -rf RS2_OK76385_PK678063_DK606752_FQ2_20080415_143807_HH_VV_HV_VH_SLC.zip
+rm -rf s2 scm scm_fr scm_fr_t4 scm_fr_t4_fl
