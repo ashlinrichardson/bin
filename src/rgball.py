79447a9300c5bf73657ec6000cc673de27b2cfb0
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 12:12:53 -0700

    update

--- src/rgball.py
+++ src/rgball.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+print("open all rgb.bin files in subdirectories, and display them one by one using cmd-line utility: imv")
+
 files = os.popen('find ./ -name "rgb.bin"').readlines()
 
 for f in files:
