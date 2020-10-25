6a7bae2f628595ddb916d5912303b72e768edae9
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 12:33:59 -0700

    update

--- src/read_multi_all.py
+++ src/read_multi_all.py
@@ -5,7 +5,9 @@ import sys
 files = os.popen('find ./ -name "rgb.bin"').readlines()
 
 py = '~/GitHub/bcws-psu-research/py/read_multi.py'
-
+if not os.path.exists(py):
+    print("Error: can't find file:", py)
+    sys.exit(1)
 
 for f in files:
     f = f.strip()
