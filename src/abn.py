82c97831cc9b70f1ca9b89be1708161d75b250c4
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-24 23:20:51 -0700

    update

---
+++ src/abn.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+args = sys.argv
+
+if len(args) < 2:
+    print("usage: abn [header file name]")
+    sys.exit(1)
+if not os.path.exists(args[1]):
+    print("Error: abn [header file name]")
+    sys.exit(1)
+
+data = open(args[1]).read()
+
+open(args[1], "wb").write( (data + "\nband names = {Band 1,\nBand 2,\nBand 3}").encode())
---
+++ src/im.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+# add band name string to end of header, if it doesn't exist, before running imv
+
+args = sys.argv
+
+if len(args) < 2:
+    print("Error: im [input binary file name]")
+    sys.exit(1)
+
+f = args[1]
+if not os.path.exists(f):
+    print("Error: input file does not exist:" + f)
+    sys.exit(1)
+
+hfn = f + '.hdr'
+h1 = hfn
+if not os.path.exists(hfn):
+    if f[-4:] == '.bin':
+        hfn = f[:-4] + '.hdr'
+if not os.path.exists(hfn):
+    print("Could not find header at: " + h1)
+    print("Could not find header at: " + hfn)
+    sys.exit(1)
+
+print(hfn)
+
+lines = open(hfn).readlines()
+
+bn = False
+for i in range(0, len(lines)):
+    if len(lines[i].split("band names")) > 1:
+        bn = True
+
+if not bn:
+    open(hfn, "wb").write(('\n'.join(lines) + '\nband names = {Band 1,\nBand 2,\nBand 3}').encode())
+
+a = os.system("imv " + f)
