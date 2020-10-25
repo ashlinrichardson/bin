3e2392080914b8451dfbf636efca82a60c9f1223
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 14:54:34 -0700

    update

--- src/abn.py
+++ src/abn.py
@@ -4,7 +4,7 @@ import sys
 args = sys.argv
 
 if len(args) < 2:
-    print("usage: abn [header file name] # add band names to header file for rgb.bin")
+    print("usage: abn [header file name] # [optional number of bands parameter]  # add band names to header file for rgb.bin")
     sys.exit(1)
 if not os.path.exists(args[1]):
     print("Error: abn [header file name]")
@@ -12,4 +12,22 @@ if not os.path.exists(args[1]):
 
 data = open(args[1]).read()
 
-open(args[1], "wb").write( (data + "\nband names = {Band 1,\nBand 2,\nBand 3}").encode())
+nb = 3
+try:
+    nb = int(args[2])
+except Exception:
+    print("Error: number of bands needs to be a number")
+
+if nb == 3:
+    open(args[1], "wb").write( (data + "\nband names = {Band 1,\nBand 2,\nBand 3}").encode())
+elif nb > 3:
+    f = open(args[1], "wb")
+    f.write(data.encode())
+    f.write("band names = {Band 1,\n".encode())
+    for i in range(1, nb - 1):
+        f.write(("Band " + str(i + 1) + ",\n").encode())
+    f.write(("Band " + str(nb) + "}").encode())
+else:
+    print("Error: less than 3 bands not supported")
+
+    
