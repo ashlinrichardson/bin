91818c786cc80de2ada2bc54b18e9ceb0a7b3229
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-25 21:19:19 -0700

    update

--- src/bin2png.py
+++ src/bin2png.py
@@ -5,10 +5,9 @@ files = [x.strip() for x in os.popen("ls -1 *.bin").readlines()]
 
 cmds = []
 for f in files:
-    cmd = "read_multi " + f + " 1" 
+    cmd = "/home/" + os.popen("whoami").read().strip() + "/GitHub/bcws-psu-research/py/read_multi.py " + f + " 1" 
     cmds.append(cmd)
 
 open("bin2png.sh", "wb").write(('\n'.join(cmds)).encode())
 
 a = os.system("multicore ./bin2png.sh")
-print(os.popen("which read_multi").read())
