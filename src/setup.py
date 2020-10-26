05e2e14c48e5f427bdea60f05ef5809dc082e0cb
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-26 11:12:51 -0700

    update

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,20 @@ import os
 import sys
 from fl0w3r import normpath, run
 src_dir = normpath(os.path.dirname(__file__))
+print("src_dir", src_dir)
+w = src_dir.strip(os.path.sep).split(os.path.sep)[:-2]
+w.append('bin')
+w.append('bin')
+export_dir = os.path.sep + os.path.sep.join(w)
+export = 'export PATH=$PATH:'  + export_dir
+
+bash_rc = '/home/' + os.popen('whoami').read().strip() + '/.bashrc'
+d = open(bash_rc).read().rstrip()
+if len(d.split(export)) < 2:
+    d += '\n' + export
+    open(bash_rc, 'wb').write(d.encode())
+
+a = os.system('bash; source ' + bash_rc)
 
 # install gsl on Ubuntu machine (still need to implement this for others)
 if os.popen('uname -a').read().strip().split()[0] == 'Ubuntu':
@@ -39,3 +53,7 @@ for afiles in [pyfiles, cfiles, cppfiles]:
         if(f.split('/')[-1] != '__init__.py'):
             f = normpath(f)
             run(cmpl + ' ' + f)
+
+
+
+
