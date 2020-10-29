77825dfd9d06f36e4c27bbbb8b3b1cd930188751
Ashlin Richardson <richardson.ashlin@gmail.com>
2020-10-28 22:45:33 -0700

    update

--- src/modf.cpp
+++ src/modf.cpp
@@ -5,18 +5,31 @@
 #include <time.h>
 #include "radlib.h"
 #include "envi.h"
-
 int main(int argc, char ** argv){
   int n_bins;
 
-  if(argc < 7){
+  if(argc < 6){
     printf("modf.cpp: Histogram binning mode filter on binary single-precision floating point file\n");
     printf("By A. Richardson, 20090821 revised 20170703;\n");
-    printf("\n\tUsage: modf [input file] [path to directory containing config.txt file] [Number of Histogram bins] [output file] [window size]\n");
+    printf("\n\tUsage: modf [input file] [path to directory containing config.txt file] [Number of Histogram bins] [output file] [window size] [ optional ratio parameter, for what?\n");
     printf("\nNote: config.txt file must be present\n");
     exit(1);
 
   }
+  int q;
+  const char * a[] = {
+	  "modf.exe",
+	  "input file",
+	  "path to folder w config.txt",
+	  "number of hist bins",
+	  "output file",
+	  "window size",
+	  "optional ratio parameter"};
+
+  for(q = 0; q < argc; q++){
+    printf("%d [%s] [%s] \n", q, argv[q], a[q]); 
+  }
+
   float ratio = 1.;
   if(argc > 6){
     ratio = atof(argv[6]);
@@ -43,6 +56,7 @@ int main(int argc, char ** argv){
     exit(1);
   }
 
+ printf("here\n");
   getT3_image_Dimensions(argv[2], NRow, NCol);
   int windowsize = atoi(argv[5]);
   if(((windowsize - 1) < 2) || (((windowsize - 1) % 2) != 0)){
@@ -50,7 +64,7 @@ int main(int argc, char ** argv){
     exit(1);
   }
   int dw = (windowsize - 1) / 2;
-  SA<float> * dat = new SA<float>(NRow * NCol);
+  float * dat = (float *)(void *) malloc(sizeof(float) * NRow * NCol); //SA<float> * dat = new SA<float>(NRow * NCol);
 
   float fmax, fmin, d;
   fmax = fmin = 0.;
@@ -60,7 +74,8 @@ int main(int argc, char ** argv){
   SA<int> hist(n_bins);
   int idx = 0;
 
-  fread(dat, sizeof(float), size, infile);
+  size_t nr = fread(dat, sizeof(float), size, infile);
+  printf("%zu\n", nr);
   int totaln = 0;
 
   int row, col, ind, max, maxi;
@@ -81,15 +96,16 @@ int main(int argc, char ** argv){
         for(j = col - dw; j < col + dw; j++){
           if((i >= 0) && (j >= 0) && (i < NRow) && (j < NCol)){
             ind = i * NCol + j;
-            d = (*dat)[ind];
+            d = dat[ind];
             if(!(isinf(d) || isnan(d))){
               idx = (int)floor(d * ((float)n_bins));
               if(idx == n_bins){
                 idx = idx - 1;
               }
               hist[idx]++;
-            }
+	    }
           }
+	  
         }
       }
       /* find largest bin */
@@ -108,6 +124,6 @@ int main(int argc, char ** argv){
   printf("\r\n");
   fclose(infile);
   fclose(outfile);
-  write_envi_hdr(out_file, NRow, NCol);
+  write_envi_hdr( (string(out_file) + string(".hdr")).c_str(), NRow, NCol);
   return 0;
 }
