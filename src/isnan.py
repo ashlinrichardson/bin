3f8d83fc025ae48554aa336624d9491cedc24a70
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-27 23:14:14 -0800

    update

---
+++ src/isnan.py
@@ -0,0 +1,24 @@
+# supposing float image, arbitrary shape, output mask band
+import os
+import sys
+import numpy as np
+args = sys.argv
+
+if len(args) < 2:
+    print("Error: usage:\n  isnan [input type4 binary file]"); sys.exit(1)
+
+if not os.path.exists(args[1]): 
+    print("Error: failed to open input file: ", args[1]); sys.exit(1)
+
+data = np.fromfile(args[1], '<f4')
+
+for i in range(0, len(data)):
+    if math.isnan(data[i]) or math.isinf(data[i]):
+        data[i] = 1.
+    else:
+        data[i] = 0.
+
+of = args[1][:-4] + "_isnan.bin"
+print("+w ", of)
+data.tofile(of, '', '<f4')
+
