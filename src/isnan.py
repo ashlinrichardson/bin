b4c8d9218d50d0551d33a6911f56f2a2218d8ed0
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-27 23:24:14 -0800

    update

--- src/isnan.py
+++ src/isnan.py
@@ -5,23 +5,17 @@ import math
 import numpy as np
 args = sys.argv
 
-if len(args) < 2:
-    print("Error: usage:\n  isnan [input type4 binary file]"); sys.exit(1)
+def err(m):
+    print("Error:", m)
+    sys.exit(1)
 
-if not os.path.exists(args[1]): 
-    print("Error: failed to open input file: ", args[1]); sys.exit(1)
-
+if len(args) < 2: err("usage:\n  isnan [input type4 binary file]")
+if not os.path.exists(args[1]): err("failed to open input file: " + args[1])
 data = np.fromfile(args[1], '<f4')
-
-for i in range(0, len(data)):
-    if math.isnan(data[i]) or math.isinf(data[i]):
-        data[i] = 1.
-    else:
-        data[i] = 0.
-
+data = np.array([1. if (math.isnan(i) or math.isinf(i)) else 0. for i in data])
 of = args[1][:-4] + "_isnan.bin"
 print("+w ", of)
 data.tofile(of, '', '<f4')
-oh =args[1][:-4] + "_isnan.hdr"
+oh = args[1][:-4] + "_isnan.hdr"
 print("+w ", oh)
 a = os.system("cp " + args[1][:-4] + ".hdr " + oh)
