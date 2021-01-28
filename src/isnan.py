a28cdbd97744ee1d9c73a2a75937c72ae9b6b01d
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-01-27 23:25:54 -0800

    update

--- src/isnan.py
+++ src/isnan.py
@@ -13,7 +13,7 @@ if len(args) < 2: err("usage:\n  isnan [input type4 binary file]")
 if not os.path.exists(args[1]): err("failed to open input file: " + args[1])
 data = np.fromfile(args[1], '<f4')
 for i in range(len(data)):
-    data[i] = 1. if (math.isnan(i) or math.isinf(i)) else 0. for i in data
+    data[i] = 1. if (math.isnan(data[i]) or math.isinf(data[i])) else 0. 
 of = args[1][:-4] + "_isnan.bin"
 print("+w ", of)
 data.tofile(of, '', '<f4')
