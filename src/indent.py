39a49a03922286217c77ac11e860074de259053d
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-06 14:00:13 -0800

    update

--- src/indent.py
+++ src/indent.py
@@ -1,13 +1,17 @@
-# add four spaces to the start of each line. Remove right trailing whitespace
+'''indent text file: add four spaces to the start of each line..
+.. Remove right trailing whitespace'''
 import os
 import sys
 import shutil
 args = sys.argv
 f_i = args[1] # input file
 f_b = args[1] + '.bak' # backup original file
+
 print("+r", f_i)
 lines = [line.rstrip() for line in open(f_i).readlines()]
+
 print(" ".join(["cp", f_i, f_b]))
 shutil.copyfile(f_i, f_b)
+
 print("+w", f_i)
 open(f_i, 'wb').write(('\n'.join(['    ' + line for line in lines])).encode())
