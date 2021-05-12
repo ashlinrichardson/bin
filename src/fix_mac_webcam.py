dd1ef1af4bf143b2e34d92f6ad9722fc1d30fccd
ashlinrichardson <richardson.ashlin@gmail.com>
2021-05-12 02:13:34 -0700

    fix mac webcam when there is <<there is no connected camera>> error

---
+++ src/fix_mac_webcam.py
@@ -0,0 +1,9 @@
+'''20210512 fix_mac_webcam.py
+
+fix a mac webcam when you get "there is no connected camera" error'''
+
+import os
+print("stopping drivers..")
+a = os.system("sudo killall VDCAssistant")
+a = os.system("sudo killall AppleCameraAssistant")
+print("now restart your app that used the webcam..")`
