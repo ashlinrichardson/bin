dbf6f01b824ce7481290733be1cc485ed6269aff
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-28 00:07:56 -0700

    update

--- src/cp_2_t2.cpp
+++ src/cp_2_t2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char ** argv){
   if(argc < 5){
-    error("cp_2_t2.cpp: convert two complex channel (ENVI type 6), implemented 20210527. \n\tuse: cp_2_t2.exe [nrow] [ncol] [ch] [cv]\n\tNote: config.txt file must be present in input directory\n");
+    error("cp_2_t2.cpp: convert two complex channel (ENVI type 6), implemented 20210527. \n\tuse: cp_2_t2.exe [nrow] [ncol] [ch] [cv] \n\tNote: config.txt file must be present in input directory\n");
   }
   size_t sf = sizeof(float);
   int nrow, ncol, row, col, i, j, k, ind;
@@ -19,8 +19,7 @@ int main(int argc, char ** argv){
   printf("nrow %d ncol %d infile %s\n", nrow, ncol, infn);
 
   string outfn(string(infn) + string(".abs"));
-  float real, imag, absv;
-  float rea2, ima2;
+  float a, b, c, d;
 
 
   FILE * infile = open(infn);
@@ -36,25 +35,40 @@ int main(int argc, char ** argv){
   }
 
 
-  FILE * outfile = wopen(outfn.c_str());
-  if(!outfile){
+  FILE * t11 = wopen("T11.bin");
+  if(!t11){
     printf("Error: could not open file %s\n", outfile);
     exit(1);
   }
+  FILE * t22 = wopen("T22.bin");
+  FILE * t12_r = wopen("T12_real.bin");
+  FILE * t12_i = wopen("T12_imag.bin");
+
 
   for0(row, nrow){
     if(nrow % 100 ==0) printf("\rProcessing row %d of %d ", row + 1, nrow);
     for0(col, ncol){
-      fread(&real, sf, 1, infile);
-      fread(&imag, sf, 1, infile);
-      fread(&rea2, sf, 1, infil2);
-      fread(&ima2, sf, 1, infil2);
-      absv = (float)(sqrt( sq(real) + sq(imag)));
-      fwrite(&absv, sf, 1, outfile);
+      fread(&a, sf, 1, infile);
+      fread(&b, sf, 1, infile);
+      fread(&c, sf, 1, infil2);
+      fread(&d, sf, 1, infil2);
+      
+      float T11 = a*a + b*b;
+      float T22 = c*c + d*d;
+      float T12_r = ac + db;
+      float T12_i = bc - da;
+
+      fwrite(&T11, sf, 1, t11);
+      fwrite(&T22, sf, 1, t22);
+      fwrite(&T12_r, sf, 1, t12_r);
+      fwrite(&T12_i, sf, 1, t12_i);
     }
   }
   printf("\r");
-  fclose(outfile);
+  fclose(t11);
+  fclose(t22);
+  fclose(t12_r);
+  fclose(t12_i);
   write_envi_hdr(outfn + string(".hdr"), nrow, ncol);
   return 0;
 }
