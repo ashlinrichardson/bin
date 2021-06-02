bce503445694a6a4fe52d57cfd5399272d3fc7d4
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-02 15:51:31 -0700

    update

--- src/qs.cpp
+++ src/qs.cpp
@@ -5,70 +5,81 @@
 #include <time.h>
 #include "radlib.h"
 
-int main(int argc, char ** argv){
+size_t size(FILE * f){
+  fseek(f, 0L, SEEK_END);
+  size_t sz = ftell(f);
+  rewind(f);
+  return sz;
+}
 
+int main(int argc, char ** argv){
   if(argc < 2){
     printf("quickstats.cpp: Quick statistics on binary single-precision floating point file. By A. Richardson, 20090424 revised 20170703.\n");
     printf("\n\tUsage: quickstats [input file] [path to directory containing config.txt file (defaults to present dir if absent)]\n");
     printf("\nNote: config.txt file must be present\n");
     exit(1);
   }
+  printf("Max,Min,Mean,Stdv\n");
 
   FILE *infile;
-  int NRow, NCol;
+  int NRow, NCol, NBand, k;
   size_t i;
+
+  getT3_image_Dimensions(argc > 2 ? argv[2] : "./", NRow, NCol);
 
   if(!(infile = fopen(argv[1], "rb"))){
     printf("Could not open input file: %s\n",argv[1]);
     exit(1);
   }
-
-  getT3_image_Dimensions(argc > 2 ? argv[2] : "./", NRow, NCol);
-  // printf("nrow %d ncol %d\n", NRow, NCol);
+ 
+  size_t f_s = size(infile);
+  NBand = f_s / (NRow * NCol); // infer number of bands from file size
   SA<float> * dat = new SA<float>(NRow*NCol);
-  for(int i = 0; i < NRow*NCol; i++){
-    (*dat)[i] = 0.;
-  }
 
-  float fmax, fmin;
-  register float d;
-  fmax = 0.;
-  fmin = 0.;
-  double total = 0.;
-  double total_squared_dif = 0.0;
-  double dif = 0.;
+  for(k = 0; k < NBand; k++){
+    for(int i = 0; i < NRow*NCol; i++) (*dat)[i] = 0.;
+    
+    float fmax, fmin;
+    register float d;
+    fmax = 0.;
+    fmin = 0.;
+    double total = 0.;
+    double total_squared_dif = 0.0;
+    double dif = 0.;
 
-  int size = NRow * NCol;
+    int size = NRow * NCol;
 
-  size_t nr = fread(dat->elements, sizeof(float), size, infile);
-  if(nr != size){
-    printf("Error: read: %d expected: %d\n", nr, size);
-    exit(1);
+    size_t nr = fread(dat->elements, sizeof(float), size, infile);
+    if(nr != size){
+      printf("Error: read: %d expected: %d\n", nr, size);
+      exit(1);
     }
-  fmax = FLT_MIN;
-  fmin = FLT_MAX;
-  int totaln = 0;
-  for(i = 0; i < size; i++){
-    d = (*dat)[i];
-    if(!(isinf(d) || isnan(d))){
-      totaln++;
-      total += d;
-      if(d < fmin) fmin = d;
-      if(d > fmax) fmax = d;
+    fmax = FLT_MIN;
+    fmin = FLT_MAX;
+    int totaln = 0;
+    for(i = 0; i < size; i++){
+      d = (*dat)[i];
+      if(!(isinf(d) || isnan(d))){
+        totaln++;
+        total += d;
+        if(d < fmin) fmin = d;
+        if(d > fmax) fmax = d;
+      }
     }
-  }
-  double avg = total / ((double)totaln);
+    double avg = total / ((double)totaln);
 
-  for(i = 0; i < size; i++){
-    d = (*dat)[i];
-    if(!(isinf(d) || isnan(d))){
-      dif = d - avg;
-      total_squared_dif += dif * dif;
+    for(i = 0; i < size; i++){
+      d = (*dat)[i];
+      if(!(isinf(d) || isnan(d))){
+        dif = d - avg;
+        total_squared_dif += dif * dif;
+      }
     }
+    double stdev = total_squared_dif / ((double)totaln);
+    stdev = sqrt(stdev);
+    printf("\n%e,%e,%e,%e", fmax, fmin, avg, stdev);
   }
-  double stdev = total_squared_dif / ((double)totaln);
-  stdev = sqrt(stdev);
-  printf("Max,Min,Mean,Stdv\n%e,%e,%e,%e\n", fmax, fmin, avg, stdev);
   fclose(infile);
+  printf("\n");
   return 0;
 }
