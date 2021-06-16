9d1e64e75e2acf54e844b52527d3b4599a4a8307
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-15 21:47:38 -0700

    update

--- src/eh2cfg.py
+++ src/eh2cfg.py
@@ -4,13 +4,15 @@ import sys
 from envi import read_envi_hdr
 from fl0w3r import args, write_config, error
 
-if len(args) < 3:
+if len(args) < 2:
 	error('eh2cfg.py: read lines/samples from envi header file, output to' + 
         ' polsarpro config.txt file ' +
 	      'Usage: [envi header file name] [path to polsarpro config file')
+
+cfg = "./config.txt" if len(args) < 3 else args[2]
 
 # read envi header
 nlin, ncol, nband = read_envi_hdr(args[1])
 
 # write image dimensions to PolSARPRo config file
-write_config(args[2], nlin, ncol)
+write_config(cfg, nlin, ncol)
