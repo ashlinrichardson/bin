0de3b9e70c37998a267bb4d272e15e88f474a382
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-18 20:59:16 -0700

    update

--- src/list.cpp
+++ src/list.cpp
@@ -1,6 +1,7 @@
-/* list files / directories (starting at a path)
- Adapted from code by Dainis Dauners / Date: 02.05.2010
- modified by A. Richardson 20210618 */
+/* list files / directories (starting at a path)..basically a "raw" find command
+
+Adapted from code by Dainis Dauners / Date: 02.05.2010
+..revised by A. Richardson 20210618 */
 #include<sys/stat.h>
 #include<iostream>
 #include<dirent.h>
@@ -14,28 +15,26 @@ bool is_dir(string dir){
   return (bool)S_ISDIR(fileInfo.st_mode);
 }
 
-void listFiles(string base_dir, bool recursive){
+void list_files(string base_dir, bool recursive){
   DIR *dp;
   struct dirent *dirp;
-  if((dp = opendir(base_dir.c_str())) == NULL)
+  if((dp = opendir(base_dir.c_str())) == NULL){
     return; // error, could not open:
+  } 
   else{
     while((dirp = readdir(dp)) != NULL){
       if(dirp->d_name != string(".") && dirp->d_name != string("..")){
-        cout << base_dir << dirp->d_name << endl;
-        if(is_dir(base_dir + dirp->d_name) == true && recursive == true){
-          listFiles(base_dir + dirp->d_name + "/", true);
-        }
+        printf("%s%s\n", base_dir.c_str(), dirp->d_name.c_str());
+        if(is_dir(base_dir + dirp->d_name) == true && recursive == true)
+          list_files(base_dir + dirp->d_name + "/", true);
       }
     }
-    closedir(dp);
   }
+  closedir(dp);
 }
 
 int main(int argc, char *argv[]){
-  if(argc <= 1)
-    listFiles(string("./"), true);
-  else
-    listFiles(argv[1], true);
+  if(argc <= 1) listFiles(string("./"), true);
+  else listFiles(argv[1], true);
   return 0;
 }
