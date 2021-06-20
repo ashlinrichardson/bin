3cdda045804c5fc87f965c7f75d148c9bb81b881
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-06-19 20:43:28 -0700

    update

--- src/cloudeT3.py
+++ src/cloudeT3.py
@@ -10,12 +10,17 @@ def run(c):
 
 args = sys.argv
 if len(args) < 2:
-    print("Usage: cloudeT4 [input directory]")
+    print("Usage: cloudeT4 [input directory] # [window size]")
     print("Generates the polarimetric decomposition parameters of the following binaries:")
     print("\tpolsarpro403/Soft/data_process_sngl/h_a_alpha_decomposition_T4.exe")
     print("\tpolsarpro403/Soft/data_process_sngl/h_a_alpha_eigenvalue_set_T4.exe")
     print("\tpolsarpro403/Soft/data_process_sngl/h_a_alpha_eigenvector_set_T4.exe")
     sys.exit(1)
+
+nwin = 1
+if len(args) > 2:
+    nwin = int(args[2])
+nwin = str(nwin)
 
 PSPexeDIR = "/home/" + os.popen("whoami").read().strip() + "/GitHub/PolSARpro_v4.2.0_Install_Linux/Soft/data_process_sngl/" #"../../PolSARpro_v4.2.0_Install_Linux/Soft/data_process_sngl/"
 
@@ -36,9 +41,9 @@ od3 = cwd+out3+'/'
 #h_a_alpha_eigenvalue_set_T4 in_dir out_dir Nwin offset_lig offset_col sub_nlig sub_ncol eigen1234 proba1234 anisotropy anisotropy12 anisotropy34 asymetry polarisation_fraction erd rvi pedestal shannon lueneburg
 #h_a_alpha_eigenvector_set_T4 in_dir out_dir Nwin offset_lig offset_col sub_nlig sub_ncol alpha1234 beta1234 epsilon1234 delta1234 gamma1234 nhu1234 alpbetepsdelgamnhu
 
-cmd1 = PSPexeDIR+out1+".exe " + cwd +" "+od1+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 "
-cmd2 = PSPexeDIR+out2+".exe " + cwd +" "+od2+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 1 1 "
-cmd3 = PSPexeDIR+out3+".exe " + cwd +" "+od3+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 "
+cmd1 = PSPexeDIR+out1+".exe " + cwd +" "+od1 + " " + nwin + " 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 "
+cmd2 = PSPexeDIR+out2+".exe " + cwd +" "+od2 + " " + nwin + " 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 1 1 "
+cmd3 = PSPexeDIR+out3+".exe " + cwd +" "+od3 + " " + nwin + " 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 "
  
 # A processing error occured ! 
 # h_a_alpha_eigenvalue_set_T3 in_dir out_dir Nwin offset_lig offset_col sub_nlig sub_ncol eigen123 proba123 anisotropy anisotropy12 asymetry polarisation_fraction erd rvi pedestal shannon lueneburg
