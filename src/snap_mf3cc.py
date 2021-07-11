4964ef320226198355c2f974d481b3aab74f9fe2
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-07-10 21:17:14 -0700

    update

--- src/snap_mf3cc.py
+++ src/snap_mf3cc.py
@@ -14,16 +14,28 @@ def run(c):
     if a != 0:
         err("command failed:\n\t" + c)
 
-if len(args) < 2:
-    print("snap_m3fcc")
+print("snap_m3fcc # needs to be run from inside a .dim folder for compact-pol data")
 
 ''' input files:'''
-
 ins = ['i_RCH2.img', 'i_RCH.img', 'q_RCH2.img', 'q_RCH.img']
 for i in ins:
     if not os.path.exists(i):
         err("req'd input file: " + i)
+if not os.path.exists("i_RCH.hdr"):
+    err("req'd input file: i_RCH.hdr")
 
 run("snap2psp ./")
 
+'''output files from snap2psp'''
+bins = ['i_RCH2.bin',  'i_RCH.bin', 'q_RCH2.bin', 'q_RCH.bin']
 
+nrow, ncol = None, None
+lines = [x.strip() for x in open('i_RCH.hdr').readlines()]
+for line in lines:
+    w = [x.strip() for x in line.split("=")]
+    if w[0] == 'samples':
+        ncol = int(w[1])
+    if w[0] == 'lines':
+        nrow = int(w[1])
+
+print("nrow", nrow, "ncol", ncol)
