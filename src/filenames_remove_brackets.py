8807aa765a16550882a1debee185313e13a16a6c
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-09-14 09:33:41 -0700

    update

---
+++ src/filenames_remove_brackets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+''' remove brackets from filenames in current directory tree'''
+import os
+import sys
+
+def err(m):
+    print("Error: " + m)
+    sys.exit(1)
+
+sep = os.path.sep
+normpath = os.path.normpath
+
+d = os.getcwd()
+
+for root, dirs, files in os.walk(d):
+    root = normpath(root)
+    if len(root.split(' ')) > 1:
+        err("space detected in path:\n" + root)
+
+for root, dirs, files in os.walk(d):
+    root = normpath(root)
+    if root[-1] != sep:
+        root += sep
+
+    for f in files:
+        ff = root + f
+
+        for c in ['[', ']', '{', '}', '(', ')']:
+            if len(f.split(c)) > 1:
+                fff = root + f.replace(c, '')
+                print('os.rename("' + ff + '",\n          "' + fff + '")')
+                os.rename(ff, fff)
