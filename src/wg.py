10670faeeebe711fef84e3e2eac5681e5272366e
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-26 16:07:42 -0700

    update

--- src/wg.py
+++ src/wg.py
@@ -1,12 +1,13 @@
 # webget-ish thing.. 20211026
 import os
 import sys
-import urllib
+import urllib.request
 args = sys.argv
+
+import xml.etree.ElementTree as ET
+
 with urllib.request.urlopen(args[1]) as reply:
-        print(reply)
-sys.exit(1)
-
-
-
-
+    reply = reply.read() # should have infix operator for this!
+    tree = ET.parse(reply)
+    root = tree.getroot()
+    print(root)
