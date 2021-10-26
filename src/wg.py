5d3a4e23d2a0087897c9bfbbbfe1a40a7dde8ecd
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-26 16:16:26 -0700

    update

--- src/wg.py
+++ src/wg.py
@@ -4,13 +4,16 @@ import re
 import sys
 import urllib.request
 args = sys.argv
+arg = args[1]
 
 import xml.etree.ElementTree as ET
 
-with urllib.request.urlopen(args[1]) as reply:
-    reply = reply.read().decode('utf-8') # should have infix operator for this!
-    print(reply)
+sep = os.path.sep 
+key = arg.strip().split(sep)[-1]
 
+with urllib.request.urlopen(arg) as reply:
+    reply = reply.read().decode('utf-8') # should have infix operator for this!
     links = re.findall("href=[\"\'](.*?)[\"\']", reply)
     for link in links:
-        print(link)
+        if len(link.split(key)) > 1:
+            print([link])
