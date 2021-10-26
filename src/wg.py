8a7ec56b5bb7817eda2d1248809f0beed6f7a3f4
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-26 16:39:11 -0700

    update

--- src/wg.py
+++ src/wg.py
@@ -5,15 +5,28 @@ import sys
 import urllib.request
 args = sys.argv
 arg = args[1]
-
-import xml.etree.ElementTree as ET
-
 sep = os.path.sep 
-key = arg.strip().split(sep)[-1]
+key = arg.strip().strip(sep).split(sep)[-1]
+
+def afind(x): return re.findall("href=[\"\'](.*?)[\"\']", x)
+
+def run(x):
+    print(x); a = os.system(x); return(a)
 
 with urllib.request.urlopen(arg) as reply:
     reply = reply.read().decode('utf-8') # should have infix operator for this!
-    links = re.findall("href=[\"\'](.*?)[\"\']", reply)
+    links = afind(reply)
     for link in links:
-        if len(link.split(key)) > 1:
-            print([link])
+        w = link.split(key)
+        if len(w) > 1:
+            tgt = arg + w[-1].strip(sep) + sep
+            reply = urllib.request.urlopen(tgt).read().decode('utf-8')
+            links2 = afind(reply)
+            for link2 in links2:
+                link2 = link2.strip()
+                if link2[-3:] == 'zip':
+                    w2 = link2.strip().split(sep)[1]
+                    pre = arg.split(w2)
+                    tgt2 = pre[0].strip(sep) + link2
+                    a = run('wget --continue ' + tgt2)         
+
