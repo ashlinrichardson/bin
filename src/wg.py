660df890b1a7eac3c7546ba53395e5ce78631e32
ashlinrichardson <richardson.ashlin@gmail.com>
2021-10-26 16:02:31 -0700

    update

--- src/wg.py
+++ src/wg.py
@@ -1,9 +1,9 @@
 # webget-ish thing.. 20211026
 import os
 import sys
-import urllib2
+import urllib
 args = sys.argv
-with urllib2.request.urlopen(args[1]) as reply:
+with urllib.request.urlopen(args[1]) as reply:
         print(reply)
 sys.exit(1)
 
