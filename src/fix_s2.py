e7580db7baa2b6b31c484b3fa6b1b7d91fd4b920
ashlinrichardson <richardson.ashlin@gmail.com>
2021-11-21 12:55:30 -0800

    update

---
+++ src/fix_s2.py
@@ -0,0 +1,17 @@
+'''add folders expected by sen2cor'''
+import os
+import sys
+args = sys.argv
+sep = os.path.sep
+
+f = args[1]
+
+def mk(f):
+    if not os.path.exists(f):
+        os.mkdir(f)
+
+mk(f + sep + 'AUX_DATA')
+mk(f + sep + 'HTML')
+
+
+
