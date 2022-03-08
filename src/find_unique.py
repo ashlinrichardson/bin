cfb4fe9bc5e33d3ba61c616b21905a810a757204
ashlinrichardson <richardson.ashlin@gmail.com>
2022-03-07 19:52:15 -0800

    update

---
+++ src/find_unique.py
@@ -0,0 +1,24 @@
+'''20220307 Group results of find command, into equivalence classes
+(by file name not incl. path)
+
+Optional argument: whether to display the locations where each filename exist
+(could also do this by piping into a "unique" command..
+..next time actually should implement unique.cpp to take stdin for unix pipe)
+
+Usage:
+    python3 find_unique.py [pattern e.g. PRS*zip for prisma data]'''
+import os
+import sys
+lines = [x.strip()
+         for x in os.popen('find ./ -name "' +
+                           args[1] +
+                           '"').read().strip().split('\n')]
+
+for line in lines:
+    print(line)
+
+
+
+
+
+
