3daaedcfb1c98cc14025dff8a9b0cfb3567e3b31
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-08 11:20:32 -0700

    sort sentinel2 filenames in common pattern from stdin

---
+++ src/s2s.py
@@ -0,0 +1,28 @@
+'''
+s2s.py e.g.:
+    find ./ -name "*.bin" | s2s
+'''
+import os
+import sys
+sep = os.path.sep
+lines = []
+for line in sys.stdin:
+    lines.append(line.strip().replace('./', ''))
+
+X = []
+for line in lines:
+    ds = None
+    w = line.split(sep)
+    for i in w:
+        if i[:2] == 'S2':
+            v = i.split('_')
+            ds = v[2]
+            X.append([ds, line])
+            break
+    if ds is None:
+        print('Error: Sentinel2 date string not found')
+        sys.exit(1)
+X.sort()
+
+for x in X:
+    print(x[1])
