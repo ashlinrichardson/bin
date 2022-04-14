038460b563837a4d2c69f6d6348e65d70bf635bc
ashlinrichardson <richardson.ashlin@gmail.com>
2022-04-13 19:56:10 -0700

    update

---
+++ src/iv.py
@@ -0,0 +1,11 @@
+''' 20220413 iv.py: feed parameters to imv in sequence
+e.g.
+find ./ -name "*swir.bin_20m.bin" | iv
+'''
+import os
+import sys
+sep = os.path.sep
+lines = []
+for line in sys.stdin:
+    lines.append(line.strip().replace('./', ''))
+    print('imv ' + line.strip())
