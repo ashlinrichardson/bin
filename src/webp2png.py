c4ed8a11b857ab433758957114ea3f5888c51a2d
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-21 23:12:04 -0700

    update

---
+++ src/webp2png.py
@@ -0,0 +1,8 @@
+import os
+import sys
+files = os.popen('ls -1 *.webp').readlines()
+for f in files:
+    f = f.strip()
+    cmd = 'convert "' + f + '" "' + f[:-4] + 'png"'
+    print(cmd)
+    a = os.system(cmd)
