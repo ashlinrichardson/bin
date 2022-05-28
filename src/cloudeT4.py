bb5b76abef6cfa8cbb2ab08fc0e0acf7f141515b
ashlinrichardson <richardson.ashlin@gmail.com>
2022-05-27 22:42:03 -0700

    adapt for polsarpro 5

--- src/cloudeT4.py
+++ src/cloudeT4.py
@@ -13,13 +13,13 @@ if len(args) < 2:
     print("\tpolsarpro403/Soft/data_process_sngl/h_a_alpha_eigenvector_set_T4.exe")
     sys.exit(1)
 
-PSPexeDIR = "/home/" + os.popen("whoami").read().strip() + "/" + "GitHub/PolSARpro_v4.2.0_Install_Linux/Soft/data_process_sngl/"
+PSPexeDIR = "/home/" + os.popen("whoami").read().strip() + "/" + "GitHub/polsarpro/Soft/bin/data_process_sngl/" # "GitHub/PolSARpro_v4.2.0_Install_Linux/Soft/data_process_sngl/"
 cwd = args[1]
 nlin, ncol = read_config(cwd + "config.txt")
 
-out1 = "h_a_alpha_decomposition_T4"
-out2 = "h_a_alpha_eigenvalue_set_T4"
-out3 = "h_a_alpha_eigenvector_set_T4"
+out1 = "h_a_alpha_decomposition"
+out2 = "h_a_alpha_eigenvalue_set"
+out3 = "h_a_alpha_eigenvector_set"
 
 od1 = cwd+out1+'/'
 od2 = cwd+out2+'/'
@@ -29,9 +29,9 @@ od3 = cwd+out3+'/'
 #h_a_alpha_eigenvalue_set_T4 in_dir out_dir Nwin offset_lig offset_col sub_nlig sub_ncol eigen1234 proba1234 anisotropy anisotropy12 anisotropy34 asymetry polarisation_fraction erd rvi pedestal shannon lueneburg
 #h_a_alpha_eigenvector_set_T4 in_dir out_dir Nwin offset_lig offset_col sub_nlig sub_ncol alpha1234 beta1234 epsilon1234 delta1234 gamma1234 nhu1234 alpbetepsdelgamnhu
 
-cmd1 = PSPexeDIR+out1+".exe " + cwd +" "+od1+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 "
-cmd2 = PSPexeDIR+out2+".exe " + cwd +" "+od2+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 1 1 1 1 1"
-cmd3 = PSPexeDIR+out3+".exe " + cwd +" "+od3+" 1 0 0 "+str(nlin)+" "+str(ncol)+" 1 1 1 1 1 1 1 "
+cmd1 = PSPexeDIR+out1+".exe -iodf T4 -id " + cwd +" -od " + od1 + " -ofr 0 -ofc 0 -nwr 1 -nwc 1 -fnr " + str(nlin)+" -fnc "+str(ncol)+" -fl1 1 -fl2 1 -fl3 1 -fl4 1 -fl5 1 -fl6 1 -fl7 1 -fl8 1 -fl9 1"
+cmd2 = PSPexeDIR+out2+".exe -iodf T4 -id " + cwd +" -od " + od2 + " -ofr 0 -ofc 0 -nwr 1 -nwc 1 -fnr " + str(nlin)+" -fnc "+str(ncol)+" -fl1 1 -fl2 1 -fl3 1 -fl4 1 -fl5 1 -fl6 1 -fl7 1 -fl8 1 -fl9 1 -fl10 1 -fl11 1 -fl12 1"
+cmd3 = PSPexeDIR+out3+".exe -iodf T4 -id " + cwd +" -od " + od3 + " -ofr 0 -ofc 0 -nwr 1 -nwc 1 -fnr " + str(nlin)+" -fnc "+str(ncol)+" -fl1 1 -fl2 1 -fl3 1 -fl4 1 -fl5 1 -fl6 1 -fl7 1 -fl8 1 -fl9 1"
  
 f = open("cloudeT4.sh", "wb");
 
