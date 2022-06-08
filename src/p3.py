91c8762329f0698922de257bca4758a14e2c4911
ashlinrichardson <richardson.ashlin@gmail.com>
2022-06-08 14:28:21 -0700

    update

--- src/p3.py
+++ src/p3.py
@@ -5,7 +5,7 @@ import array
 from fl0w3r import error, run, normpath, args
 
 if len(args) < 3:
-    errror("pos_v5.py: POS Polarization Orientation Shifts Correction T3" +
+    error("pos_v5.py: POS Polarization Orientation Shifts Correction T3" +
            " Version, Impl by A. Richardson 200712, revised 20170628" +
            "Usage: p3 [in_dir] [out_dir]")
 
@@ -19,22 +19,22 @@ t3_outdir = normpath(args[2])
 extract_chnl, pos_pal_cmd = "p3ec", "p3c"
 run("cp " + t3_indir + "config.txt " + t3_outdir + "config.txt")
 
-print "-------------------------------------------------------------------"
-print "STEP 1"
-print pos_pal_cmd
-print "-------------------------------------------------------------------"
+print("-------------------------------------------------------------------")
+print("STEP 1")
+print(pos_pal_cmd)
+print("-------------------------------------------------------------------")
 run(pos_pal_cmd+" "+t3_indir+" "+t3_outdir)
 run("cp "+t3_indir+"config.txt "+t3_outdir+"config.txt")
 
-print "-------------------------------------------------------------------"
-print "STEP 2"
-print extract_chnl
-print "-------------------------------------------------------------------"
+print("-------------------------------------------------------------------")
+print("STEP 2")
+print(extract_chnl)
+print("-------------------------------------------------------------------")
 run(extract_chnl+" "+t3_indir+" "+t3_outdir)
 run("mv -v theta " + t3_outdir)
 
-print "-------------------------------------------------------------------"
-print "OUTPUT FILES"
-print t3_indir
-print t3_outdir
-print "-------------------------------------------------------------------"
+print("-------------------------------------------------------------------")
+print("OUTPUT FILES")
+print(t3_indir)
+print(t3_outdir)
+print("-------------------------------------------------------------------")
