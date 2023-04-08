95a424710f258866e8ee8d5143dfe06dfeacd701
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-08 10:14:19 -0700

    update

--- src/mp42png.py
+++ src/mp42png.py
@@ -1,9 +1,14 @@
-'''convert mp4 file to individual jpeg using ffmpeg'''
+'''convert mp4 file to individual png using ffmpeg'''
 import os
 import cv2
 import sys
 import math
 args = sys.argv
+
+if not os.path.exists(args[1]):
+    print("usage: mp42png.py [input mp4 video file]")
+    sys.exit(1)
+
 cap = cv2.VideoCapture(args[1])
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 zeros = math.ceil(math.log(frame_count) / math.log(10)) + 1
