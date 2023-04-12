d3ff40792f52451bd2bfc680f34b8028dccc4898
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-12 11:08:54 -0700

    update

--- src/pngadd.py
+++ src/pngadd.py
@@ -7,10 +7,15 @@ lines = [x.strip() for x in os.popen("ls -1 *.png").readlines()]
 
 print(lines[0])
 im = np.array(Image.open(lines[0]))
-
+im = im.astype('float')
 for i in range(1, len(lines)):
     print(lines[i])
-    im += np.array(Image.open(lines[i]))
+    x = np.array(Image.open(lines[i]))
+    x = x.astype('float')
+    im += x
 
-im = Image.fromarray(im)
-im.save("sum.png")
+im = im * 255. / np.amax(im)
+im = im.astype(np.uint8)
+
+x = Image.fromarray(im)
+x.save("sum.png")
