3c67e7dd521a5e7ed4739e87246c085e94cbb4e3
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-21 22:56:06 -0700

    update

--- src/mp42png.py
+++ src/mp42png.py
@@ -5,15 +5,16 @@ import sys
 import math
 args = sys.argv
 
-if not os.path.exists(args[1]):
-    print("usage: mp42png.py [input mp4 video file]")
+if len(args) < 2 or not os.path.exists(args[1]):
+    print("usage: mp42png.py [input mp4 video file] # [optional arg: transpose]") 
     sys.exit(1)
+transpose = '' if len(args) < 3 else ' -vf "transpose=clock" '
 
 cap = cv2.VideoCapture(args[1])
 frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print("frame count:", frame_count)
 
 zeros = math.ceil(math.log(frame_count) / math.log(10)) + 1
-cmd = "ffmpeg -i " + args[1] + " " + args[1] + "_%0" + str(zeros) + "d.png"
+cmd = "ffmpeg -i "+ args[1] + " " + transpose + " " + args[1] + "_%0" + str(zeros) + "d.png"
 print(cmd)
 a = os.system(cmd)
