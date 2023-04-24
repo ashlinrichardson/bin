ea12cb65bce2fe538ea9fffbdc3fbfff390815f1
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-04-24 15:40:18 -0700

    update

---
+++ src/delete_network_bridge.py
@@ -0,0 +1,11 @@
+'''20230424 delete network bridge e.g. found by ifconfig'''
+import os
+import sys
+
+def run(c):
+    print(c)
+    return os.system(c)
+
+x = sys.argv[1]
+run('sudo ip link set ' + x + ' down')
+run('sudo brctl delbr ' + x)
