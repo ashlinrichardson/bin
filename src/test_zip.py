72c864a364c890f9cc94b81f26fda76ce6a1f42b
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-05-27 15:07:00 -0700

    update

---
+++ src/test_zip.py
@@ -0,0 +1,18 @@
+'''20230527 test integrity of zip files in subfolders
+'''
+import os
+import sys
+
+lines = os.popen('find ./ -name "*.zip"').readlines()
+lines = [x.strip() for x in lines]
+
+for line in lines:
+    cmd = 'zip -T ' + line
+    print(cmd)
+
+
+
+
+
+
+
