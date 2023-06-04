ab1ce86125194bedd302ca4ded091e9f82aade75
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-03 21:36:25 -0700

    update

--- src/delete_network_bridge.py
+++ src/delete_network_bridge.py
@@ -7,6 +7,14 @@ def run(c):
     print(c)
     return os.system(c)
 
-x = sys.argv[1]
-run('sudo ip link set ' + x + ' down')
-run('sudo brctl delbr ' + x)
+bridges = None
+if len(sys.argv) > 1:
+    bridges = [sys.argv[1]]
+
+else:
+    lines = [x.strip().split() for x in os.popen('brctl show').readlines()]
+    bridges = [x[0] for x in lines]
+
+for b in bridges:
+    run('sudo ip link set ' + x + ' down')
+    run('sudo brctl delbr ' + x)
