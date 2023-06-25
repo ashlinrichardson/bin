fa19bc1bf874252560deb94c205854f5fda3ec06
Ashlin Richardson <ashlin.richardson@gmail.com>
2023-06-25 15:12:14 -0700

    update

--- src/heic2jpg.py
+++ src/heic2jpg.py
@@ -1,9 +1,12 @@
 import os
 import sys
 
+def run(c):
+    print(c); return os.system(c)
+
 lines = [x.strip() for x in os.popen("ls -1 *.heic").readlines()]
 
 for line in lines:
     cmd = 'heif-convert ' + line + ' ' + line[:-5] + '.jpg'
-    print(cmd)
+    run(cmd)
 
