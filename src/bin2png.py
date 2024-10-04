41a8f8f04153b1a03a9c125eae58326c0afc3768
Ashlin Richardson <ashlin.richardson@gmail.com>
2024-10-04 12:48:02 -0700

    update

--- src/bin2png.py
+++ src/bin2png.py
@@ -5,7 +5,7 @@ files = [x.strip() for x in os.popen("ls -1 *.bin").readlines()]
 
 cmds = []
 for f in files:
-    cmd = "/home/" + os.popen("whoami").read().strip() + "/GitHub/bcws-psu-research/py/read_multi.py " + f + " 1" 
+    cmd = "/home/" + os.popen("whoami").read().strip() + "/GitHub/wps-research/py/read_multi.py " + f + " 1" 
     cmds.append(cmd)
 
 open("bin2png.sh", "wb").write(('\n'.join(cmds)).encode())
